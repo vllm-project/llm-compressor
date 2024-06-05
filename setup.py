46987afeae6884c24567bc65a23230aa72b84210
Benjamin Fineran <bfineran@users.noreply.github.com>
2024-06-05 16:49:44 -0400

    remove pydantic version cap (#80)
    * remove pydantic version cap
    
    * add floor to 2.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def _setup_packages() -> List:
     )
     
 def _setup_install_requires() -> List:
-    return ["torch>=1.7.0", "transformers", "pydantic<2.7"]
+    return ["torch>=1.7.0", "transformers", "pydantic>=2.0"]
 
 def _setup_extras() -> Dict:
     return {"dev": ["black==22.12.0", "isort==5.8.0", "wheel>=0.36.2", "flake8>=3.8.3", "pytest>=6.0.0", "nbconvert>=7.16.3"]}
