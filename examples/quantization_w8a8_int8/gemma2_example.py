67292387020276b38cd18cd66bf3833147a9ca91
Robert Shaw <rshaw@neuralmagic.com>
2024-08-11 22:57:42 +0000

    gemma2

--- examples/quantization_w8a8_int8/gemma2_example.py
+++ examples/quantization_w8a8_int8/gemma2_example.py
@@ -2,26 +2,22 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 
 from llmcompressor.modifiers.quantization import GPTQModifier
-from llmcompressor.modifiers.smoothquant import SmoothQuantModifier
 from llmcompressor.transformers import SparseAutoModelForCausalLM, oneshot
 
-# Select model and load it.
-MODEL_ID = "meta-llama/Meta-Llama-3-8B-Instruct"
+# 1) Select model and load it.
+MODEL_ID = "google/gemma-2-2b-it"
 model = SparseAutoModelForCausalLM.from_pretrained(
-    MODEL_ID,
-    device_map="auto",
-    torch_dtype="auto",
-)
+    MODEL_ID, device_map="auto", torch_dtype="auto",)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 
-# Select calibration dataset.
+# 2) Prepare calibration dataset.
 DATASET_ID = "HuggingFaceH4/ultrachat_200k"
 DATASET_SPLIT = "train_sft"
 
 # Select number of samples. 512 samples is a good place to start.
 # Increasing the number of samples can improve accuracy.
 NUM_CALIBRATION_SAMPLES = 512
-MAX_SEQUENCE_LENGTH = 1024
+MAX_SEQUENCE_LENGTH = 2048
 
 # Load dataset and preprocess.
 ds = load_dataset(DATASET_ID, split=DATASET_SPLIT)
@@ -53,34 +49,25 @@ def tokenize(sample):
 
 ds = ds.map(tokenize, remove_columns=ds.column_names)
 
-# Configure algorithms. In this case, we:
-#   * apply SmoothQuant to make the activations easier to quantize
+# 3) Select quantization algorithms. In this case, we:
 #   * quantize the weights to int8 with GPTQ (static per channel)
 #   * quantize the activations to int8 (dynamic per token)
 # Note: set sequential_update: true in the recipe to reduce memory
-recipe = [
-    SmoothQuantModifier(smoothing_strength=0.8),
-    GPTQModifier(targets="Linear", scheme="W8A8", ignore=["lm_head"]),
-]
+recipe = GPTQModifier(targets="Linear", scheme="W8A8", ignore=["lm_head"])
 
-# Apply algorithms.
+# 4) Apply quantization and save to disk compressed.
 oneshot(
     model=model,
     dataset=ds,
     recipe=recipe,
     max_seq_length=MAX_SEQUENCE_LENGTH,
     num_calibration_samples=NUM_CALIBRATION_SAMPLES,
+    output_dir=MODEL_ID.split("/")[1] + "-INT8"
 )
 
 # Confirm generations of the quantized model look sane.
-print("\n\n")
 print("========== SAMPLE GENERATION ==============")
 input_ids = tokenizer("Hello my name is", return_tensors="pt").input_ids.to("cuda")
-output = model.generate(input_ids, max_new_tokens=100)
+output = model.generate(input_ids, max_new_tokens=20)
 print(tokenizer.decode(output[0]))
-print("==========================================\n\n")
-
-# Save to disk compressed.
-SAVE_DIR = MODEL_ID.split("/")[1] + "-W8A8-Dynamic-Per-Token"
-model.save_pretrained(SAVE_DIR, save_compressed=True)
-tokenizer.save_pretrained(SAVE_DIR)
+print("==========================================")
