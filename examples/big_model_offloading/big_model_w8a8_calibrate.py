d74c2cfe2013e850a5128cfaf51cbc8ffbcc6666
Sara Adkins <sara@neuralmagic.com>
2024-08-12 12:05:02 -0400

    Layer by Layer Sequential GPTQ Updates (#47)
    * WIP
    
    * cleanup, disable cache
    
    * docstrings
    
    * move to layer compressor
    
    * small fixes
    
    * docstrings
    
    * fix offloading
    
    * remove example debugging
    
    * fix structure check
    
    * update example back to 70b
    
    * remove debugging code
    
    * remove debug code
    
    * fix unit tests
    
    * update example
    
    * update example
    
    * reert test change
    
    * example tweak

--- examples/big_model_offloading/big_model_w8a8_calibrate.py
+++ examples/big_model_offloading/big_model_w8a8_calibrate.py
@@ -2,42 +2,24 @@ import torch
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
+from llmcompressor.modifiers.quantization import GPTQModifier
 from llmcompressor.transformers import SparseAutoModelForCausalLM, oneshot
 from llmcompressor.transformers.compression.helpers import calculate_offload_device_map
 
-# define a llmcompressor recipe for FP8 quantization
-# this recipe requires calibration
-recipe = """
-quant_stage:
-    quant_modifiers:
-        GPTQModifier:
-            sequential_update: true
-            ignore: ["lm_head"]
-            config_groups:
-                group_0:
-                    weights:
-                        num_bits: 8
-                        type: int
-                        strategy: tensor
-                        dynamic: false
-                        symmetric: true
-                    input_activations:
-                        num_bits: 8
-                        type: float
-                        strategy: tensor
-                        dynamic: false
-                        symmetric: true
-                    targets: ["Linear"]
-"""
+# define a llmcompressor recipe for W8A8 quantization
+recipe = GPTQModifier(
+    sequential_update=True, targets="Linear", scheme="W8A8", ignore=["lm_head"]
+)
 
 model_stub = "meta-llama/Meta-Llama-3-70B-Instruct"
 
+# adjust based off number of desired GPUs
 device_map = calculate_offload_device_map(
     model_stub, reserve_for_hessians=True, num_gpus=2, torch_dtype=torch.float16
 )
 
 model = SparseAutoModelForCausalLM.from_pretrained(
-    model_stub, torch_dtype=torch.float16, device_map=device_map
+    model_stub, torch_dtype=torch.bfloat16, device_map=device_map
 )
 tokenizer = AutoTokenizer.from_pretrained(model_stub)
 output_dir = "./output_llama3b_70b_w8a8"
@@ -45,8 +27,8 @@ output_dir = "./output_llama3b_70b_w8a8"
 # Select calibration dataset.
 DATASET_ID = "HuggingFaceH4/ultrachat_200k"
 DATASET_SPLIT = "train_sft"
-NUM_CALIBRATION_SAMPLES = 4
-MAX_SEQUENCE_LENGTH = 512
+NUM_CALIBRATION_SAMPLES = 512
+MAX_SEQUENCE_LENGTH = 2048
 
 # Load dataset and preprocess.
 ds = load_dataset(DATASET_ID, split=DATASET_SPLIT)
@@ -77,7 +59,6 @@ def tokenize(sample):
 
 
 ds = ds.map(tokenize, remove_columns=ds.column_names)
-
 
 oneshot(
     model=model,
