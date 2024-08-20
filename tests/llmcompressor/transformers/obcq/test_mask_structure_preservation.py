fb846908528b65d7ebbe45d27678e5cfc08e13b8
Dipika Sikka <dipikasikka1@gmail.com>
2024-08-20 18:46:48 -0400

    Update test_mask_structure_preservation.py

--- tests/llmcompressor/transformers/obcq/test_mask_structure_preservation.py
+++ tests/llmcompressor/transformers/obcq/test_mask_structure_preservation.py
@@ -9,7 +9,7 @@ from llmcompressor.core import reset_session
 from tests.testing_utils import parse_params, requires_torch
 
 MASK_STRUCTURE_CONFIGS_DIRECTORY = (
-    "llm-compressor/tests/llmcompressor/transformers/obcq/obcq_configs/consec_runs/mask_structure"
+    "tests/llmcompressor/transformers/obcq/obcq_configs/consec_runs/mask_structure"
 )
 
 
