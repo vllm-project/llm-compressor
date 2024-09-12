add7d32903526c4515577002cd38d606a23b54ab
Kyle Sayers <kyle@neuralmagic.com>
2024-09-12 19:10:37 +0000

    load offload state dict

--- src/llmcompressor/transformers/sparsification/compressed_tensors_utils.py
+++ src/llmcompressor/transformers/sparsification/compressed_tensors_utils.py
@@ -73,6 +73,10 @@ def modify_save_pretrained(model: PreTrainedModel):
             # state_dict gets passed in as a kwarg for FSDP models
             state_dict = kwargs.get("state_dict", None)
 
+            # find offloaded state dict
+            if state_dict is None:
+                state_dict = get_state_dict_offloaded_model(model)
+
             if sparsity_config is not None:
                 sparsity_config.global_sparsity = (
                     SparsityConfigMetadata.infer_global_sparsity(
@@ -112,12 +116,6 @@ def modify_save_pretrained(model: PreTrainedModel):
                     save_directory, **kwargs
                 )
                 return
-
-            # if we've gotten to this point we have a config so we can run compression
-            # default safe serialization to True if not explicitly set
-            kwargs["safe_serialization"] = kwargs.get("safe_serialization", True)
-            if state_dict is None:
-                state_dict = get_state_dict_offloaded_model(model)
 
             # make sure we're on the main process when saving
             if state_dict is not None and len(state_dict) > 0:
