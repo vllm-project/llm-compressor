7a112d65fcc11370695643e1cf783d520e6c0f38
Rahul Tuli <rahul@neuralmagic.com>
2024-09-18 00:22:45 +0000

    Some cleanup

--- src/llmcompressor/transformers/finetune/data/pile.py
+++ src/llmcompressor/transformers/finetune/data/pile.py
@@ -4,7 +4,7 @@ from typing import Optional
 from llmcompressor.transformers.finetune.data import TextGenerationDataset
 
 
-@TextGenerationDataset.register(name="mit-han-lab/pile-val-backup", alias=["pile-eval"])
+@TextGenerationDataset.register(name="pile_eval")
 class PileEvalDataset(TextGenerationDataset):
     """
     Child text generation class for the PileEval dataset
