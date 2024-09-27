ae5a2290f64783b854ed4b2d8289723959c0fcba
kylesayrs <kyle@neuralmagic.com>
2024-09-27 14:47:11 +0000

    move definition

--- src/llmcompressor/transformers/__init__.py
+++ src/llmcompressor/transformers/__init__.py
@@ -6,6 +6,7 @@ Tools for integrating LLM Compressor with transformers training flows
 
 # isort: skip_file
 # (import order matters for circular import avoidance)
+from .wrap import wrap_hf_model_class
 from .utils import *
-from .sparsification import SparseAutoModel, SparseAutoModelForCausalLM, wrap_hf_model_class
+from .sparsification import SparseAutoModel, SparseAutoModelForCausalLM
 from .finetune import *
