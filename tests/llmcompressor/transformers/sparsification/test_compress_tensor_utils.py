5cb95b77efdbfa8f5a47507f193586ed58be10f0
Kyle Sayers <kylesayrs@gmail.com>
2024-09-29 16:55:07 -0400

    Rename to quantization config (#730)
    * use quantization config, apply style
    
    * remove style changes
    
    ---------
    
    Co-authored-by: kylesayrs <kyle@neuralmagic.com>

--- tests/llmcompressor/transformers/sparsification/test_compress_tensor_utils.py
+++ tests/llmcompressor/transformers/sparsification/test_compress_tensor_utils.py
@@ -3,7 +3,7 @@ import shutil
 
 import pytest
 import torch
-from compressed_tensors import COMPRESSION_CONFIG_NAME
+from compressed_tensors import QUANTIZATION_CONFIG_NAME
 from compressed_tensors.compressors import ModelCompressor
 from compressed_tensors.config import BitmaskConfig, DenseSparsityConfig
 from compressed_tensors.quantization import QuantizationStatus
@@ -75,7 +75,7 @@ def test_sparse_model_reload(compressed, config, dtype, tmp_path):
     )
 
     config = AutoConfig.from_pretrained(tmp_path / "compress_out")
-    compression_config = getattr(config, COMPRESSION_CONFIG_NAME, None)
+    compression_config = getattr(config, QUANTIZATION_CONFIG_NAME, None)
     sparsity_config = ModelCompressor.parse_sparsity_config(compression_config)
     assert (
         sparsity_config["format"] == "dense"
@@ -126,7 +126,7 @@ def test_dense_model_save(tmp_path, skip_compression_stats, save_compressed):
 
     # for models with 0% sparsity no sparsity config is saved regardless
     config = AutoConfig.from_pretrained(tmp_path / "dense_out")
-    compression_config = getattr(config, COMPRESSION_CONFIG_NAME, None)
+    compression_config = getattr(config, QUANTIZATION_CONFIG_NAME, None)
     sparsity_config = ModelCompressor.parse_sparsity_config(compression_config)
     assert sparsity_config is None
 
@@ -185,7 +185,7 @@ def test_quant_model_reload(format, dtype, tmp_path):
     )
 
     config = AutoConfig.from_pretrained(tmp_path / "compress_out")
-    compression_config = getattr(config, COMPRESSION_CONFIG_NAME, None)
+    compression_config = getattr(config, QUANTIZATION_CONFIG_NAME, None)
     quant_config = ModelCompressor.parse_quantization_config(compression_config)
     assert quant_config["format"] == format
 
