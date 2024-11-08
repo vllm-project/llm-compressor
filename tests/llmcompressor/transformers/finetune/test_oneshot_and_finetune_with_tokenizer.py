7dee6c5be6dc155859d14f713c0d0cebf3b36a12
Dipika <dipikasikka1@gmail.com>
2024-11-08 18:30:47 +0000

    expose one gpu for finetune; update to use a better moodel and show generation for completeness

--- tests/llmcompressor/transformers/finetune/test_oneshot_and_finetune_with_tokenizer.py
+++ tests/llmcompressor/transformers/finetune/test_oneshot_and_finetune_with_tokenizer.py
@@ -20,12 +20,17 @@ class TestOneshotAndFinetuneWithTokenizer(unittest.TestCase):
 
     def setUp(self):
         self.output = "./finetune_output"
+        # finetune workflows in general seem to have trouble with multi-gpus
+        # use just one atm
+        self.monkeypatch = pytest.MonkeyPatch()
 
     def test_oneshot_and_finetune_with_tokenizer(self):
         from datasets import load_dataset
         from transformers import AutoTokenizer
 
         from llmcompressor.transformers import SparseAutoModelForCausalLM, compress
+
+        self.monkeypatch.setenv("CUDA_VISIBLE_DEVICES", "0")
 
         recipe_str = (
             "tests/llmcompressor/transformers/finetune/test_alternate_recipe.yaml"
@@ -60,5 +65,12 @@ class TestOneshotAndFinetuneWithTokenizer(unittest.TestCase):
             tokenizer=tokenizer,
         )
 
+        input_ids = tokenizer("Hello my name is", return_tensors="pt").input_ids.to(
+            "cuda"
+        )
+        output = model_loaded.generate(input_ids, max_new_tokens=100)
+        print(tokenizer.decode(output[0]))
+
     def tearDown(self):
         shutil.rmtree(self.output)
+        self.monkeypatch.undo()
