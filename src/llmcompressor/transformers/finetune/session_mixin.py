1d1407e0f538fdae4ac2c46a387f583272e6f961
Imran Ullah <103720343+imrankh46@users.noreply.github.com>
2024-11-27 17:29:46 +0530

    Update session_mixin.py
    replace tokenizer by processor
    
    ```processor = self.processing_class if hasattr(self, "processing_class") else self.tokenizer
    if processor is not None:
        processor.save_pretrained(output_dir)```

--- src/llmcompressor/transformers/finetune/session_mixin.py
+++ src/llmcompressor/transformers/finetune/session_mixin.py
@@ -486,8 +486,11 @@ class SessionManagerMixIn:
             )
 
         self.save_state()
-        if self.tokenizer is not None:
-            self.tokenizer.save_pretrained(output_dir)
+        processor = self.processing_class if hasattr(self, "processing_class") else self.tokenizer
+        if processor is not None:
+            processor.save_pretrained(output_dir)
+        # if self.tokenizer is not None:
+        #     self.tokenizer.save_pretrained(output_dir)
 
         if not self.recipe:
             return
