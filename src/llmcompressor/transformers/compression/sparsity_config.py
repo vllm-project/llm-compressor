938a526a9c9393f497862b5125d16ce2da0c2776
Rahul Tuli <rahul@neuralmagic.com>
2024-12-05 02:58:00 +0000

    Bitmask test

--- src/llmcompressor/transformers/compression/sparsity_config.py
+++ src/llmcompressor/transformers/compression/sparsity_config.py
@@ -102,10 +102,8 @@ class SparsityConfigMetadata:
             # compression
             format = CompressionFormat.dense.value
         if compress:
-            if sparsity_structure == SparsityStructure.TWO_FOUR.value:
-                format = CompressionFormat.sparse_24.value
-            else:
-                format = CompressionFormat.sparse_bitmask.value
+            format = CompressionFormat.sparse_bitmask.value
+
         else:
             format = CompressionFormat.dense.value
 
