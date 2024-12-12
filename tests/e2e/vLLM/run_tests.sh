8de35a400b2fb79b490fba5240fcdd8e9cee1038
George Ohashi <george@neuralmagic.com>
2024-12-12 12:44:03 -0500

    revert

--- tests/e2e/vLLM/run_tests.sh
+++ tests/e2e/vLLM/run_tests.sh
@@ -16,35 +16,27 @@ while getopts "c:t:" OPT; do
   esac
 done
 
-# Function to process a file
-process_model_config() {
-    local MODEL_CONFIG=$1
+# Parse list of configs.
+for MODEL_CONFIG in "$CONFIG"/*
+do
     LOCAL_SUCCESS=0
-    
+
     echo "=== RUNNING MODEL: $MODEL_CONFIG ==="
-    export TEST_DATA_FILE=${MODEL_CONFIG}
+
+    export TEST_DATA_FILE="$MODEL_CONFIG"
     pytest \
-        -r a \
         --capture=tee-sys \
         --junitxml="test-results/e2e-$(date +%s).xml" \
         "$TEST" || LOCAL_SUCCESS=$?
 
     if [[ $LOCAL_SUCCESS == 0 ]]; then
-        echo "=== PASSED MODEL: ${MODEL_CONFIG} ==="
+        echo "=== PASSED MODEL: $MODEL_CONFIG ==="
     else
-        echo "=== FAILED MODEL: ${MODEL_CONFIG} ==="
+        echo "=== FAILED MODEL: $MODEL_CONFIG ==="
     fi
-    SUCCESS=$((SUCCESS + LOCAL_SUCCESS))
-}
 
-# Use find to locate all files in MODEL_CONFIGS and process them
-find "$MODEL_CONFIGS" -type f | while read -r MODEL_CONFIG
-do
-    process_model_config "$MODEL_CONFIG"
+    SUCCESS=$((SUCCESS + LOCAL_SUCCESS))
+
 done
 
-if [ "${SUCCESS}" -eq "0" ]; then
-    exit 0
-else
-    exit 1
-fi
+exit "$SUCCESS"
