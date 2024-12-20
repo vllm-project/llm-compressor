a0a6cf7e33bc8c8181f249a65c5ba6c9651f48ed
Kyle Sayers <kylesayrs@gmail.com>
2024-12-20 21:53:42 +0000

    add no copyright to hf files
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/transformers/tracing/mllama.py
+++ src/llmcompressor/transformers/tracing/mllama.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# vllm-project: no copyright
 """PyTorch Mllama model."""
 
 import math
