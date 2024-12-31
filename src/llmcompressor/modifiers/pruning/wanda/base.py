6e79ca18fc81d2ffd7c555f61c31ee9ae2afadb0
Kyle Sayers <kylesayrs@gmail.com>
2024-12-31 17:41:00 -0500

    WIP: consider extending arguments to support non-layered models
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/modifiers/pruning/wanda/base.py
+++ src/llmcompressor/modifiers/pruning/wanda/base.py
@@ -1,18 +1,17 @@
 from functools import partial
 from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
 
+import warnings
 import numpy as np
 import torch
 from loguru import logger
 from torch.nn import Module
 from tqdm import tqdm
 from collections import defaultdict
+from pydantic import field_validator, model_validator
 
 from llmcompressor.core import State
 from llmcompressor.modifiers import Modifier
-from llmcompressor.modifiers.pruning.wanda.utils.wanda_wrapper import WandaWrapper
-from llmcompressor.modifiers.utils.layer_compressor import LayerCompressor
-from llmcompressor.modifiers.utils.pytorch_helpers import run_calibration_forward
 from llmcompressor.utils.pytorch.module import (
     get_layers,
     get_no_split_params,
@@ -49,20 +48,46 @@ class WandaPruningModifier(Modifier):
         to compress every layer in the model
     """
 
-    sparsity: Union[float, List[float]] = 0.0
-    sparsity_profile: Optional[str] = None
-    owl_m: Optional[int] = None
-    owl_lmbda: Optional[float] = None
+    sparsity: Union[float, List[float]]
     mask_structure: str = "0:0"
-    sequential_update: Optional[bool] = False
+    owl_m: Optional[int] = None
+    owl_lmbda: Optional[float] = None  # misspelling?
+    sparsity_profile: Optional[str] = None  # deprecated
+
+    module_targets: Union[str, List[str], None] = None
+    targets: Union[str, List[str], None] = None  # deprecated, clones sequential_targets
     sequential_targets: Union[str, List[str], None] = None
-    model: Optional[Any] = None
 
-    compressible_layers_: Optional[List] = None
-    prunen_: Optional[int] = None
-    prunem_: Optional[int] = None
+    # private variables
+    _prunen: Optional[int] = None
+    _prunem: Optional[int] = None
 
-    # DEPRECATE sparsity_profile
+    @field_validator("sparsity_profile", mode="before")
+    def validate_sparsity_profile(cls, value) -> None:
+        if value is not None:
+            warnings.warn(
+                "`sparsity_profile` is deprecated, use `owl_m` and `owl_lmbda`"
+            )
+        return None
+
+    @field_validator("targets", mode="before")
+    def validate_targets(cls, value) -> None:
+        if value is not None:
+            warnings.warn(
+                "`targets` is deprecated, use `module_targets` and `sequential_targets`"
+            )
+        return None
+
+    @model_validator(mode="after")
+    def validate_model_after(model: "WandaPruningModifier") -> Dict[str, Any]:
+        owl_m = model.owl_m
+        owl_lmbda = model.owl_lmbda
+        mask_structure = model.mask_structure
+
+        if (owl_m is not None) ^ (owl_lmbda is not None):
+            raise ValueError("Must provide both `owl_m` and `owl_lmbda` or neither")
+        
+        model._prunen, model._prunen = mask_structure.split(":")
 
     def on_initialize(self, state: State, **kwargs) -> bool:
         """
@@ -74,34 +99,30 @@ class WandaPruningModifier(Modifier):
         model = state.model
         dataloader = state.data.calib
 
-        self._infer_mask_block_size()
+        # infer module and sequential targets
+        self.sequential_targets = self._infer_sequential_targets(model)
 
-        # infer sparsity
-        if self.owl_m is not None:
+        # infer layer sparsities
+        if self.owl_m is not None and self.owl_lmbda is not None:
             logger.info(
-                "Inferring layer-wise sparsities from "
+                "Using OWL to infer target layer-wise sparsities from "
                 f"{len(dataloader) if dataloader else 0} calibration samples..."
             )
             activations = self._get_activations(model, dataloader)
-            target_sparsities = self._infer_layer_sparsity(activations)
-        self._validate_layerwise_sparsity()
-
-        # infer sequential targets (formerly compressible layers)
-        if self.sequential_targets is None:
-            self.sequential_targets = get_no_split_params(state.model)
-        if isinstance(self.sequential_targets, str):
-            self.sequential_targets = [self.sequential_targets]
+            self.sparsity = self._infer_layer_sparsity(activations)
 
         # register hooks
-        for name, module in state.model.named_modules():
-            get_prunable_layers(self.layer)
-            if getattr_chain(module, "quantization_scheme.weights", None) is not None:
-                # HACK: previously, embeddings were not quantized because they were not
-                # accessible by the layer compressor. For now, we manually ignore it,
-                # but in the FUTURE this should be ignored by the user
-                if not isinstance(module, torch.nn.Embedding):
-                    post_hook = partial(self.compress_module, name)
-                    self.register_hook(module, post_hook, "forward")
+        for index, name, layer in enumerate(get_layers(self.sequential_targets, model)):
+            if isinstance(self.sparsity, Dict):
+                layer_sparsity = self.sparsity[name]
+            elif isinstance(self.sparsity, List):
+                layer_sparsity = self.sparsity[index]
+            else:
+                layer_sparsity = self.sparsity
+
+            for name, module in get_prunable_layers(layer):
+                post_hook = partial(self.compress_module, name, self._prunen, self._prunem, layer_sparsity)
+                self.register_hook(module, post_hook, "forward")
 
         # infer and run pipeline
         model_name = state.model.__class__.__name__
@@ -110,7 +131,7 @@ class WandaPruningModifier(Modifier):
         try:
             run_sequential(
                 state.model,
-                sequential_targets,
+                self.sequential_targets,
                 self.ignore,
                 state.data.calib,
                 propagate_error=True,
@@ -127,7 +148,7 @@ class WandaPruningModifier(Modifier):
             try:
                 run_layer_sequential(
                     state.model,
-                    sequential_targets,
+                    self.sequential_targets,
                     state.data.calib,
                     propagate_error=True,
                 )
@@ -146,49 +167,14 @@ class WandaPruningModifier(Modifier):
                 run_basic(state.model, state.data.calib)
                 return True
 
-        self.apply_compression(calibration_dataloader)
-
         return True
 
-    def _validate_layerwise_sparsity(self):
-        if isinstance(self.sparsity, float):
-            # single sparsity will be applied to all layers
-            return
+    def _infer_sequential_targets(self, model):
+        if self.sequential_targets is None:
+            return get_no_split_params(model)
+        if isinstance(self.sequential_targets, str):
+            return [self.sequential_targets]
 
-        target_layers = list(self.compressible_layers_.keys())
-
-        if len(target_layers) != len(self.sparsity):
-            raise ValueError(
-                "Number of layer targets must match the number of "
-                f"sparsities. Got {len(target_layers)} layers and "
-                f"{len(self.sparsity)} sparsities"
-            )
-
-    def _pruning_arguments(self, sparsity) -> Dict[str, Any]:
-        """
-        Gather the parameters needed for root module compression in a dict
-
-        :param sparsity: target sparsity
-        :return: dict of params for pruning
-        """
-        return {
-            "sparsity": sparsity,
-            "prunen": self.prunen_,
-            "prunem": self.prunem_,
-        }
-
-    def _infer_mask_block_size(self):
-        """
-        Infer the mask block size from the mask structure.
-        Parses mask_structure of the form N:M where N, M are integers that
-        define a custom block shape; and sets prunen_ and prunem_ accordingly.
-
-        :post-condition: prunen_ and prunem_ are set
-        """
-        if self.mask_structure is None:
-            raise ValueError("mask_structure must be defined")
-
-        self.prunen_, self.prunem_ = list(map(int, self.mask_structure.split(":")))
 
     def _infer_layer_sparsity(self, activations):
         wanda = {}
@@ -232,22 +218,24 @@ class WandaPruningModifier(Modifier):
             logger.info(f"Sparsity for {k}: {sparsities[k]}")
         return sparsities
 
-    def _get_wanda(self, model, dataloader, nsamples=128):
+    def _get_activations(self, model, dataloader, nsamples=128):
         acts = defaultdict(int)
 
         def save_acts(module, input, name):
             nonlocal acts
-
             if isinstance(input, tuple):
                 input = input[0]
             acts[name] += 1.0 / nsamples * input.pow(2).sum(dim=(0, 1)).sqrt()
 
+        # TODO: only add hooks to target modules
         hooks = [
             self.register_hook(mod, partial(save_acts, name=name), "forward_pre")
             for name, mod in self.model.named_modules()
             if isinstance(mod, torch.nn.Linear) and "lm_head" not in name
-            # This is dumb. We should only attach to the modules which will be compressed
         ]
+        # TODO: need to run but only activating these hooks
+        # TODO: need disable_hooks to be composable
+        # with HooksMixin.disable_hooks(keep=hooks)
         run_basic(model, dataloader)
         self.remove_hooks(hooks)
 
