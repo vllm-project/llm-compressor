c23913d01fda3c5adcb6b7a43f7b94453816d791
Rahul Tuli <rahul@neuralmagic.com>
2025-01-03 21:23:25 +0000

    save_compressed in fp8 cases

--- examples/2of4_sparse/llama3_8b_2of4.py
+++ examples/2of4_sparse/llama3_8b_2of4.py
@@ -116,5 +116,5 @@ print(tokenizer.decode(output[0]))
 print("==========================================\n")
 
 # Save compressed model and tokenizer
-model.save_pretrained(save_dir, save_compressed=True)
+model.save_pretrained(save_dir, save_compressed=args.fp8)
 tokenizer.save_pretrained(save_dir)
