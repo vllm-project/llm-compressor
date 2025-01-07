153a4fa3c4d4831e01219b8d7a901366555bd960
Kyle Sayers <kylesayrs@gmail.com>
2025-01-07 00:22:19 +0000

    correct typos
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/pipelines/sequential/helpers.py
+++ src/llmcompressor/pipelines/sequential/helpers.py
@@ -58,8 +58,8 @@ def trace_subgraphs(
     :param model: model being traced
     :param sample_input: inputs whose values will change during execution but whose
         __len__, __bool__, and __contains__ values are assumed constant across batches
-    :param sequential_targets: list of patterns specifying sequential targets
-    :param ignore: list of patterns specifying modules to ignore during tracing
+    :param sequential_targets: list of patterns matching sequential targets
+    :param ignore: list of patterns matching modules to ignore during tracing
     :return: a list of Subgraphs in order of execution
     """
     # find modules
@@ -191,8 +191,8 @@ def topological_partition(graph: GraphModule, targets: Set[Module]) -> List[List
 
     :param graph: graph being partitioned
     :param targets: target modules which will be assigned to disjoint partitions
-    :return: list of partitions, where each partition is a list of nodes belong to that
-        partition
+    :return: list of partitions, where each partition is a list of nodes belonging to
+        that partition
     """
     assert check_assumption(graph.graph)
     target_nodes = find_target_nodes(graph, targets)
@@ -256,7 +256,7 @@ def partition_graph(model: Module, partitions: List[List[Node]]) -> List[Subgrap
 
     :param model: model which owns the produced Subgraphs
     :param partitions: list of partitions, where each partition is a list of nodes
-        belong to that partition
+        belonging to that partition
     :return: list of subgraphs in order of execution
     """
     subgraphs = []
@@ -352,7 +352,7 @@ def check_assumption(graph: Graph) -> bool:
 
 def match_modules(model: Module, target_names: List[str]) -> Set[Module]:
     """
-    Find modules whose names matach the patterns given by `target_names`
+    Find modules whose names match the patterns given by `target_names`
 
     :param model: model containing submodules to find
     :param target_names: target patterns to find
