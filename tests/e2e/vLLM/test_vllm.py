2217f28958f50b992e170a7ac4ea877b65063337
George Ohashi <george@neuralmagic.com>
2025-01-13 12:20:07 -0500

    clear memory after each run

--- tests/e2e/vLLM/test_vllm.py
+++ tests/e2e/vLLM/test_vllm.py
@@ -168,6 +168,9 @@ class TestvLLM:
         self.tear_down()
 
     def tear_down(self):
+        import torch
+        torch.cuda.empty_cache()
+
         if self.save_dir is not None:
             shutil.rmtree(self.save_dir)
 
