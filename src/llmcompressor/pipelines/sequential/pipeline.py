ddda051242d35f38ca809cc7e3bfbd511db28878
Dipika <dipikasikka1@gmail.com>
2025-01-14 02:27:23 +0000

    rev

--- src/llmcompressor/pipelines/sequential/pipeline.py
+++ src/llmcompressor/pipelines/sequential/pipeline.py
@@ -8,7 +8,6 @@ from compressed_tensors.utils import get_execution_device
 from llmcompressor.modifiers.utils.hooks import HooksMixin
 from llmcompressor.pipelines.cache import IntermediatesCache
 from llmcompressor.pipelines.sequential.helpers import trace_subgraphs
-from llmcompressor.timer_utils import log_time
 from llmcompressor.utils.helpers import calibration_forward_context
 
 if TYPE_CHECKING:
@@ -16,10 +15,6 @@ if TYPE_CHECKING:
 
 __all__ = ["run_pipeline"]
 
-
-@log_time
-def run_forward(model, inputs, forward_function):
-    return forward_function(model, **inputs)
 
 
 def run_pipeline(
@@ -73,8 +68,7 @@ def run_pipeline(
             # do an preliminary pass to trigger modifier hooks
             for batch_index in tqdm.tqdm(range(len(dataloader)), desc=calib_desc):
                 inputs = intermediates.fetch(batch_index, subgraph.input_names)
-                run_forward(model, inputs, forward_function)
-                # forward_function(model, **inputs)
+                forward_function(model, **inputs)
 
             # TODO: replace with a lifecycle event
             if callback_modifier:
@@ -85,8 +79,7 @@ def run_pipeline(
             with HooksMixin.disable_hooks():
                 for batch_index in tqdm.tqdm(range(len(dataloader)), desc=prop_desc):
                     inputs = intermediates.fetch(batch_index, subgraph.input_names)
-                    # output = forward_function(model, **inputs)
-                    output = run_forward(model, inputs, forward_function)
+                    output = forward_function(model, **inputs)
 
                     if subgraph_index < num_subgraphs - 1:
                         intermediates.update(batch_index, output)
---
+++ src/llmcompressor/timer.py
@@ -0,0 +1,69 @@
+import logging
+import time
+from contextlib import contextmanager
+from threading import RLock
+
+import numpy
+
+__all__ = ["Timer"]
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Timer:
+    """Timer to log timings during inference requests. Should be used through
+    timings.utils.get_singleton_manager()
+    """
+
+    _instance = None
+
+    def __init__(self, enable_logging: bool):
+        """
+        :param enable_logging: whether or not time logging is enabled
+        """
+        self.enable_logging = enable_logging
+        self.measurements = {}
+        self.lock = RLock()
+
+    @contextmanager
+    def time(self, func_name: str):
+        """
+        Time the given function, add the time to the dictionary of lists
+        tracking each function and calculate the average, if
+        self.avg_after_iterations number of iterations have passed.
+
+        :param func_name: name of the function that will be used to log the
+            measurements
+        """
+        start = time.time()
+        try:
+            yield
+        finally:
+            end = time.time()
+            with self.lock:
+                self._update(func_name, end - start)
+
+    def _update(self, func_name: str, time_elapased: float):
+        """
+        Update the dictionary of measurements for each function call
+
+        :param func_name: name of the function that will be used to log the
+            measurements
+        :param time_elapsed: time taken for function execution
+        """
+        if self.measurements.get(func_name) is None:
+            self.measurements[func_name] = [time_elapased]
+        else:
+            self.measurements[func_name].append(time_elapased)
+
+    def _compute_average(self):
+        """
+        Display averages time for each logged function. After the average is
+        calculated, clear the measurements.
+
+        """
+        with self.lock:
+            for func_name in self.measurements:
+                _LOGGER.info(f"Average time for {func_name}: ")
+                _LOGGER.info(str(numpy.average(self.measurements.get(func_name))))
+                self.measurements[func_name].clear()
