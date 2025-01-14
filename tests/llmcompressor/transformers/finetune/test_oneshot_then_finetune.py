336c867b0f5bde80709d7f0aec331e7964e5c918
George <george@neuralmagic.com>
2025-01-14 22:38:53 +0000

    decompress model explicitly

--- tests/llmcompressor/transformers/finetune/test_oneshot_then_finetune.py
+++ tests/llmcompressor/transformers/finetune/test_oneshot_then_finetune.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import pytest
 from transformers import AutoModelForCausalLM
+from transformers.utils.quantization_config import CompressedTensorsConfig
 
 from llmcompressor.core import create_session
 from llmcompressor.modifiers.quantization import QuantizationModifier
@@ -21,6 +22,7 @@ from llmcompressor.transformers import oneshot, train
 class TestOneshotThenFinetune(unittest.TestCase):
     def setUp(self):
         self.output = Path("./finetune_output")
+        self.quantization_config = CompressedTensorsConfig(run_compressed=False)
 
     def test_oneshot_sparsification_then_finetune(self):
         recipe_str = "tests/llmcompressor/transformers/obcq/recipes/test_tiny2.yaml"
@@ -47,8 +49,12 @@ class TestOneshotThenFinetune(unittest.TestCase):
         recipe_str = (
             "tests/llmcompressor/transformers/finetune/test_finetune_recipe.yaml"
         )
+
+        # Explictly decompress the model for training using quantization_config
         model = AutoModelForCausalLM.from_pretrained(
-            self.output / "oneshot_out", device_map="auto"
+            self.output / "oneshot_out",
+            device_map="auto",
+            quantization_config=self.quantization_config,
         )
         distill_teacher = AutoModelForCausalLM.from_pretrained(
             "Xenova/llama2.c-stories15M", device_map="auto"
@@ -75,7 +81,10 @@ class TestOneshotThenFinetune(unittest.TestCase):
         # test reloading checkpoint and final model
         # verify checkpoint reloading and can carry out finetune
         # with the saved model
-        model = AutoModelForCausalLM.from_pretrained(output_dir, device_map="auto")
+        # Explictly decompress the model for training using quantization_config
+        model = AutoModelForCausalLM.from_pretrained(
+            output_dir, device_map="auto", quantization_config=self.quantization_config
+        )
         with create_session():
             train(
                 model=model,
