e1845995d69a99546a943fc5d1f262d54892dd65
Kyle Sayers <kylesayrs@gmail.com>
2025-01-20 15:03:59 -0500

    VLM: Phi3 Vision Example (#1032)
    ## Purpose ##
    * Provide example for quantizing phi3_vision model
    
    ## Changes ##
    * Add phi3 example script
    * When tokenizing, we have to set `writer_batch_size=1`, otherwise we
    encounter https://github.com/pola-rs/polars/issues/862
    * My suspicion is that this is due to the phi processor outputs being
    especially long
    * Add phi3 vision data collator
    
    ## Testing ##
    Model | Dataset | Scheme | Runtime | Winogrande |
    -- | -- | -- | -- | --
    Phi-3-vision-128k-instruct | flickr | W4A16 | 51m, 1xA100 | 0.7151 
    
    ---------
    
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>
    Signed-off-by: Domenic Barbuzzi <domenic@neuralmagic.com>
    Co-authored-by: George <george@neuralmagic.com>
    Co-authored-by: Dipika Sikka <dipikasikka1@gmail.com>
    Co-authored-by: Domenic Barbuzzi <domenic@neuralmagic.com>
    Co-authored-by: Michael Goin <michael@neuralmagic.com>

---
+++ examples/multimodal_vision/phi3_vision_example.py
@@ -0,0 +1,93 @@
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoProcessor
+
+from llmcompressor.modifiers.quantization import GPTQModifier
+from llmcompressor.transformers import oneshot
+from llmcompressor.transformers.utils.data_collator import phi3_vision_data_collator
+
+# Load model.
+model_id = "microsoft/Phi-3-vision-128k-instruct"
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map="auto",
+    torch_dtype="auto",
+    trust_remote_code=True,
+    _attn_implementation="eager",
+)
+processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
+processor.chat_template = processor.tokenizer.chat_template
+
+# Oneshot arguments
+DATASET_ID = "lmms-lab/flickr30k"
+DATASET_SPLIT = "test[:512]"
+NUM_CALIBRATION_SAMPLES = 512
+MAX_SEQUENCE_LENGTH = 2048
+
+# Load dataset and preprocess.
+ds = load_dataset(DATASET_ID, split=DATASET_SPLIT)
+ds = ds.shuffle(seed=42).select(range(NUM_CALIBRATION_SAMPLES))
+
+
+# Apply chat template
+def preprocess(example):
+    messages = [{"role": "user", "content": "<|image_1|>\nWhat does the image show?"}]
+    return {
+        "text": processor.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+        ),
+        "images": example["image"],
+    }
+
+
+ds = ds.map(preprocess)
+
+
+# # Tokenize inputs.
+def tokenize(sample):
+    return processor(
+        text=sample["text"],
+        images=sample["images"],
+        padding=False,
+        max_length=MAX_SEQUENCE_LENGTH,
+        truncation=True,
+    )
+
+
+# long data lengths produced by the phi3_vision processor
+# can lead to integer overflows when mapping, avoid with writer_batch_size
+ds = ds.map(tokenize, writer_batch_size=1, remove_columns=ds.column_names)
+
+
+# Recipe
+recipe = [
+    GPTQModifier(
+        targets="Linear",
+        scheme="W4A16",
+        sequential_targets=["Phi3DecoderLayer"],
+        ignore=["lm_head", "re:model.vision_embed_tokens.*"],
+    ),
+]
+
+# Perform oneshot
+oneshot(
+    model=model,
+    dataset=ds,
+    recipe=recipe,
+    max_seq_length=MAX_SEQUENCE_LENGTH,
+    num_calibration_samples=NUM_CALIBRATION_SAMPLES,
+    trust_remote_code_model=True,
+    data_collator=phi3_vision_data_collator,
+)
+
+# Confirm generations of the quantized model look sane.
+print("========== SAMPLE GENERATION ==============")
+input_ids = processor(text="Hello my name is", return_tensors="pt").input_ids.to("cuda")
+output = model.generate(input_ids, max_new_tokens=20)
+print(processor.decode(output[0]))
+print("==========================================")
+
+# Save to disk compressed.
+SAVE_DIR = model_id.split("/")[1] + "-W4A16-G128"
+model.save_pretrained(SAVE_DIR, save_compressed=True)
+processor.save_pretrained(SAVE_DIR)
