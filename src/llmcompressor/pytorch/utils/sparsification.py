15884dc39ae298fe325e632c91a819928a9a383f
George Ohashi <george@neuralmagic.com>
2025-01-22 15:52:22 -0500

    comment

--- src/llmcompressor/pytorch/utils/sparsification.py
+++ src/llmcompressor/pytorch/utils/sparsification.py
@@ -107,15 +107,10 @@ class ModuleSparsificationInfo:
         """
         num_params = 0
         for name, layer in get_quantized_layers(self.module):
-            num_param_weight = torch.numel(
-                self.trainable_params.get(f"{name}.weight", 0)
-            )
-            num_params += num_param_weight
-            if num_param_weight:
-                logger.warning(f"{name} is not recognized in trainable_params")
-                continue
-            if hasattr(layer, "bias") and layer.bias is not None:
-                num_params += layer.bias
+            if getattr(layer, "weight", None) is not None:
+                num_params += torch.numel(layer.weight)
+                if getattr(layer, "bias", None) is not None:
+                    num_params += torch.numel(layer.bias)
 
         return num_params
 
