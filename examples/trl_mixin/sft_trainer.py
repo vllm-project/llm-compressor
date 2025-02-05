be3196057c5e59da2537dd8e50e39427ad283fe6
George Ohashi <george@neuralmagic.com>
2025-02-05 16:38:11 -0500

    examples TrainingArguments movement

--- examples/trl_mixin/sft_trainer.py
+++ examples/trl_mixin/sft_trainer.py
@@ -1,7 +1,7 @@
 from trl import SFTConfig as TRLSFTConfig
 from trl import SFTTrainer as TRLSFTTrainer
 
-from llmcompressor.transformers import TrainingArguments
+from llmcompressor.transformers.utils.arg_parser import TrainingArguments
 from llmcompressor.transformers.finetune.session_mixin import SessionManagerMixIn
 
 __all__ = ["SFTTrainer"]
