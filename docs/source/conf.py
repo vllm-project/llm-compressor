3aaed23297bf7415c6125cdde1b5c68d987afa47
Kyle Sayers <kylesayrs@gmail.com>
2025-02-08 20:11:51 -0500

    add autodoc
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,9 @@ author = 'vLLM Project'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
