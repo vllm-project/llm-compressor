dd443e5a890232d3c0f7ddde9ee25af4c043fd94
Brian Dellabetta <bdellabe@redhat.com>
2025-02-11 23:18:42 +0000

    failing at lm-eval stage, index out of bounds error
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- tests/e2e/vLLM/test_vllm.py
+++ tests/e2e/vLLM/test_vllm.py
@@ -66,6 +66,7 @@ class TestvLLM:
             pytest.skip("Skipping test; cadence mismatch")
 
         self.model = eval_config["model"]
+        self.model_type = eval_config["model_type"]
         self.scheme = eval_config.get("scheme")
         self.dataset_id = eval_config.get("dataset_id")
         self.dataset_config = eval_config.get("dataset_config")
@@ -97,6 +98,7 @@ class TestvLLM:
             self.save_dir = self.model.split("/")[1] + f"-{self.scheme}"
         oneshot_model, tokenizer = run_oneshot_for_e2e_testing(
             model=self.model,
+            model_type=self.model_type,
             device=self.device,
             num_calibration_samples=self.num_calibration_samples,
             max_seq_length=self.max_seq_length,
