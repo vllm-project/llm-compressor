380d16432d826172c315d49f8c470c58bf333798
George Ohashi <george@neuralmagic.com>
2025-02-11 14:37:40 -0500

    run examples pass

--- src/llmcompressor/transformers/finetune/__init__.py
+++ src/llmcompressor/transformers/finetune/__init__.py
@@ -3,5 +3,4 @@
 from .data import TextGenerationDataset
 from .session_mixin import SessionManagerMixIn
 from .text_generation import apply, compress, eval, train
-from .training_args import TrainingArguments
 
