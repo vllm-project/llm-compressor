vllm-project/llm-compressor

commit e604f41baa14fb003664d3fb8500c822ecdff74d

[Oneshot Refactor] dataclass Arguments (#1103)
ORDER OF REVIEWS:
1. https://github.com/vllm-project/llm-compressor/pull/1108
2. https://github.com/vllm-project/llm-compressor/pull/1103 <- current
PR
3. https://github.com/vllm-project/llm-compressor/pull/1109
4. https://github.com/vllm-project/llm-compressor/pull/1110

SUMMARY:
Refactor dataclass used for llm-compressor entrypoints (oneshot, train,
apply) to decouple non-relevant attributes from the existing dataclass.
Ex. recipe in training_args. Recipe is contained in a session, not in
the trainer that training_args govern.

Dataclass refactor details are in

https://docs.google.com/document/d/1YbR1dTQmCzqhGk74m5msBzqoPHQgB6dVxDtf6cTmetc/edit?usp=sharing

Note: 
https://github.com/vllm-project/llm-compressor/pull/1110 takes care of
using a new entrypoint that will prohibit the post_train / oneshot call
to use training_args. Current entrypoint will need training_args for
oneshot to function - this PR is just for refactoring the dataclass.


Before:
ModelArguments:
https://github.com/vllm-project/llm-compressor/blob/6fa5a5eecc7d363ec73474d011d40135b6374179/src/llmcompressor/transformers/finetune/model_args.py#L6
DataTrainingArguments:
https://github.com/vllm-project/llm-compressor/blob/6fa5a5eecc7d363ec73474d011d40135b6374179/src/llmcompressor/transformers/finetune/data/data_args.py#L70
TrainingArguments:
https://github.com/vllm-project/llm-compressor/blob/6fa5a5eecc7d363ec73474d011d40135b6374179/src/llmcompressor/transformers/finetune/training_args.py#L10

After:
ModelArguments:
https://github.com/vllm-project/llm-compressor/pull/1103/files#diff-58fd0f7ae4564317960ae0d4d4b2cdb97b9588c1915f062915e74ecf51b5502cR6
DatasetArguments:
https://github.com/vllm-project/llm-compressor/pull/1103/files#diff-5e43f74ba5d8327b937adada3c7f30a7efb13f9a44cb3fdb5e1a2a12b8b8ea27R70
RecipeArguments:
https://github.com/vllm-project/llm-compressor/pull/1103/files#diff-0ff9c048a4deb55e5459054bdc61a5d8c81da9c94588ec2355e6b2c2ec8675d1R6

examples/trl_mixin/ex_trl_constant.py
@@ -3,7 +3,7 @@ from sft_trainer import SFTTrainer
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from trl import DataCollatorForCompletionOnlyLM
 
-from llmcompressor.transformers import TrainingArguments
+from llmcompressor.args import TrainingArguments
 
 model_path = "neuralmagic/Llama-2-7b-pruned50-retrained"
 output_dir = "./output_trl_sft_test_7b_gsm8k_sft_data"

examples/trl_mixin/sft_trainer.py
@@ -1,7 +1,7 @@
 from trl import SFTConfig as TRLSFTConfig
 from trl import SFTTrainer as TRLSFTTrainer
 
-from llmcompressor.transformers import TrainingArguments
+from llmcompressor.args import TrainingArguments
 from llmcompressor.transformers.finetune.session_mixin import SessionManagerMixIn
 
 __all__ = ["SFTTrainer"]

src/llmcompressor/transformers/finetune/session_mixin.py
@@ -36,8 +36,7 @@ from llmcompressor.utils.fsdp.helpers import is_fsdp_model, save_pretrained_fsdp
 from llmcompressor.utils.pytorch import qat_active
 
 if TYPE_CHECKING:
-    from llmcompressor.transformers import DataTrainingArguments
-
+    from llmcompressor.args import DatasetArguments, ModelArguments
 
 __all__ = [
     "SessionManagerMixIn",
@@ -66,14 +65,16 @@ class SessionManagerMixIn:
 
     def __init__(
         self,
-        recipe: Optional[str] = None,
-        recipe_args: Optional[Union[Dict[str, Any], str]] = None,
-        data_args: Optional["DataTrainingArguments"] = None,
+        recipe: str,
+        data_args: "DatasetArguments",
+        model_args: "ModelArguments",
         teacher: Optional[Union[Module, str]] = None,
+        recipe_args: Optional[Union[Dict[str, Any], str]] = None,
         **kwargs,
     ):
         self.recipe = recipe
         self.recipe_args = recipe_args
+        self.model_args = model_args
         self.teacher = teacher
 
         # parse training and metadata args
@@ -374,8 +375,8 @@ class SessionManagerMixIn:
         self.initialize_session(epoch=epoch, checkpoint=checkpoint, stage=stage)
 
         # do not save checkpoints as compressed
-        original_save_compressed = self.args.save_compressed
-        self.args.save_compressed = False
+        original_save_compressed = self.model_args.save_compressed
+        self.model_args.save_compressed = False
 
         # train with accelerator
         self.accelerator.wait_for_everyone()
@@ -383,7 +384,7 @@ class SessionManagerMixIn:
         self.accelerator.wait_for_everyone()
 
         # restore original setting for saving final model
-        self.args.save_compressed = original_save_compressed
+        self.model_args.save_compressed = original_save_compressed
 
         # lifecycle
         self.finalize_session()
@@ -433,7 +434,6 @@ class SessionManagerMixIn:
     ):
         """
         Run oneshot calibration on the active model
-
         :param stage: which stage of the recipe to run, or None to run whole recipe
         :param calib_data: dataloader of calibration data
         """
@@ -474,7 +474,7 @@ class SessionManagerMixIn:
         if not is_fsdp_model(self.model):
             self.model.save_pretrained(
                 output_dir,
-                save_compressed=self.args.save_compressed,
+                save_compressed=self.model_args.save_compressed,
                 safe_serialization=self.args.save_safetensors,
             )
         else:  # FSDP model
@@ -482,7 +482,7 @@ class SessionManagerMixIn:
                 model=self.model,
                 accelerator=self.accelerator,
                 output_dir=output_dir,
-                save_compressed=self.args.save_compressed,
+                save_compressed=self.model_args.save_compressed,
                 save_safetensors=self.metadata.get("save_safetensors", False),
             )
 

examples/trl_mixin/ex_trl_distillation.py
@@ -1,11 +1,8 @@
 from sft_trainer import SFTTrainer
 from transformers import AutoModelForCausalLM, AutoTokenizer, DefaultDataCollator
 
-from llmcompressor.transformers import (
-    DataTrainingArguments,
-    TextGenerationDataset,
-    TrainingArguments,
-)
+from llmcompressor.args import DatasetArguments, TrainingArguments
+from llmcompressor.transformers import TextGenerationDataset
 
 model_path = "neuralmagic/Llama-2-7b-pruned50-retrained"
 teacher_path = "neuralmagic/Llama-2-7b-gsm8k"
@@ -21,7 +18,7 @@ teacher = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 # Load gsm8k using SparseML dataset tools
-data_args = DataTrainingArguments(
+data_args = DatasetArguments(
     dataset="gsm8k", dataset_config_name="main", max_seq_length=512
 )
 dataset_manager = TextGenerationDataset.load_from_registry(

src/llmcompressor/args/__init__.py
@@ -1,0 +1,6 @@
+# flake8: noqa
+
+from .dataset_arguments import DatasetArguments
+from .model_arguments import ModelArguments
+from .recipe_arguments import RecipeArguments
+from .training_arguments import TrainingArguments

src/llmcompressor/args/dataset_arguments.py
@@ -5,7 +5,7 @@ from transformers import DefaultDataCollator
 
 
 @dataclass
-class DVCDatasetTrainingArguments:
+class DVCDatasetArguments:
     """
     Arguments for training using DVC
     """
@@ -17,7 +17,7 @@ class DVCDatasetTrainingArguments:
 
 
 @dataclass
-class CustomDataTrainingArguments(DVCDatasetTrainingArguments):
+class CustomDatasetArguments(DVCDatasetArguments):
     """
     Arguments for training using custom datasets
     """
@@ -67,10 +67,10 @@ class CustomDataTrainingArguments(DVCDatasetTrainingArguments):
 
 
 @dataclass
-class DataTrainingArguments(CustomDataTrainingArguments):
+class DatasetArguments(CustomDatasetArguments):
     """
     Arguments pertaining to what data we are going to input our model for
-    training and eval
+    calibration, training or eval
 
     Using `HfArgumentParser` we can turn this class into argparse
     arguments to be able to specify them on the command line

src/llmcompressor/args/model_arguments.py
@@ -5,7 +5,9 @@ from typing import Optional
 @dataclass
 class ModelArguments:
     """
-    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from
+    Model variables used for oneshot calibration, finetuning and
+    stage runners (sequential run of oneshot and finetune).
+
     """
 
     model: str = field(
@@ -44,17 +46,7 @@ class ModelArguments:
         default=None,
         metadata={"help": "Where to store the pretrained data from huggingface.co"},
     )
-    use_fast_tokenizer: bool = field(
-        default=True,
-        metadata={"help": "Whether to use one of the fast tokenizers. Default True"},
-    )
-    model_revision: str = field(
-        default="main",
-        metadata={
-            "help": "The specific model version to use "
-            "(can be a branch name, tag name or commit id)"
-        },
-    )
+
     use_auth_token: bool = field(
         default=False,
         metadata={
@@ -81,5 +73,20 @@ class ModelArguments:
             "help": "Whether or not to allow for custom models to execute their "
             "own modeling files. This option should only be set to True for "
             "repositories you trust and in which you have read the code"
+        },
+    )
+    save_compressed: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Whether to compress sparse models during save"},
+    )
+    oneshot_device: Optional[str] = field(
+        default="cuda:0",
+        metadata={"help": "Device to run oneshot calibration on"},
+    )
+    model_revision: str = field(
+        default="main",
+        metadata={
+            "help": "The specific model version to use "
+            "(can be a branch name, tag name or commit id)"
         },
     )

src/llmcompressor/args/recipe_arguments.py
@@ -1,0 +1,32 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+
+@dataclass
+class RecipeArguments:
+    """Recipe and session variables"""
+
+    recipe: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Path to a LLM Compressor sparsification recipe",
+        },
+    )
+    recipe_args: Optional[List[str]] = field(
+        default=None,
+        metadata={
+            "help": (
+                "List of recipe arguments to evaluate, of the format key1=value1 "
+                "key2=value2"
+            )
+        },
+    )
+    clear_sparse_session: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether to clear CompressionSession/CompressionLifecycle ",
+                "data between runs.",
+            )
+        },
+    )

commit 9258eb3e5d143b3bb38fa9abceb8da12e1e9cc08

[TRL_SFT_Trainer] Fix and Update Examples code (#1161)
SUMMARY:
* Fix examples script failure
https://github.com/neuralmagic/llm-compressor-testing/actions/runs/13350457472/job/37286313648

PROBLEM
1. 
```bash
 cpy 2 '/home/gohashi/llm-compressor/examples/trl_mixin/ex_trl_constant.py'
...
TypeError: SessionManagerMixIn.__init__() missing 2 required positional arguments: 'data_args' and 'model_args'

```

2.
```bash
(.venv) gohashi@janice:~/llm-compressor$ cpy 2 '/home/gohashi/llm-compressor/examples/trl_mixin/ex_trl_constant.py'
...
TypeError: SFTTrainer.__init__() got an unexpected keyword argument 'max_seq_length'
```

3.
```bash
(.venv) gohashi@janice:~/llm-compressor$ cpy 2 '/home/gohashi/llm-compressor/examples/trl_mixin/ex_trl_constant.py'
...
AttributeError: 'NoneType' object has no attribute 'save_compressed'
```

4.
```
(.venv) gohashi@janice:~/llm-compressor$ cpy 2 '/home/gohashi/llm-compressor/examples/trl_mixin/ex_trl_constant.py'
...
/home/gohashi/llm-compressor/src/llmcompressor/transformers/finetune/session_mixin.py:97: FutureWarning: `tokenizer` is deprecated and removed starting from version 0.16.0 for `SFTTrainer.__init__`. Use `processing_class` instead.
...
```


SOLUTION:
1. Caused by
https://github.com/vllm-project/llm-compressor/pull/1103/files#diff-059b8cf7e48691cd2d5ddda1d0ba5f584657a70c5804797d38c902b433777335R69-R70,
where `model_args` and `data_args` is required. Add it to the code and
make `model_args` and `data_args` optional
2. `max_seq_length` is not a part of `TrainingArgs`, which gets called
by super first. We see that it is used in `SFTConfig` that inherits
`TRLSFTConfig` where `max_seq_length` is used. `TRLSFTConfig` inherits
`TrainingArguments` to modify the code.
3. Make `model_args` required
4. Bug warning. Update `tokenizer` to `processing_class`

TEST PLAN:
*

examples/trl_mixin/ex_trl_constant.py
@@ -3,7 +3,7 @@ from sft_trainer import SFTTrainer
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from trl import DataCollatorForCompletionOnlyLM
 
-from llmcompressor.args import TrainingArguments
+from llmcompressor.args import ModelArguments
 
 model_path = "neuralmagic/Llama-2-7b-pruned50-retrained"
 output_dir = "./output_trl_sft_test_7b_gsm8k_sft_data"
@@ -39,21 +39,23 @@ def formatting_prompts_func(example):
 response_template = "Answer:"
 collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
 
-training_args = TrainingArguments(
+trl_sft_config_args = dict(
     output_dir=output_dir,
     num_train_epochs=0.6,
     logging_steps=50,
     gradient_checkpointing=True,
+    max_seq_length=512,
 )
+model_args = ModelArguments(model=model)
 
 trainer = SFTTrainer(
     model=model,
-    tokenizer=tokenizer,
+    processing_class=tokenizer,
     recipe=recipe,
     train_dataset=dataset,
     formatting_func=formatting_prompts_func,
     data_collator=collator,
-    args=training_args,
-    max_seq_length=512,
+    trl_sft_config_args=trl_sft_config_args,
+    model_args=model_args,
 )
 trainer.train()

examples/trl_mixin/sft_trainer.py
@@ -1,20 +1,17 @@
+from typing import Dict, Optional
+
 from trl import SFTConfig as TRLSFTConfig
 from trl import SFTTrainer as TRLSFTTrainer
 
-from llmcompressor.args import TrainingArguments
 from llmcompressor.transformers.finetune.session_mixin import SessionManagerMixIn
 
 __all__ = ["SFTTrainer"]
 
 
 class SFTTrainer(SessionManagerMixIn, TRLSFTTrainer):
-    def __init__(self, *args, **kwargs):
-        sft_config_args = kwargs.get("args")
-        if (
-            sft_config_args is not None
-            and sft_config_args.__class__.__name__ == "TrainingArguments"
-        ):
-            kwargs["args"] = SFTConfig(**sft_config_args.to_dict())
+    def __init__(self, trl_sft_config_args: Optional[Dict] = None, *args, **kwargs):
+        if trl_sft_config_args is not None:
+            kwargs["args"] = TRLSFTConfig(**trl_sft_config_args)
         super().__init__(*args, **kwargs)
 
     def _prepare_dataset(self, dataset, *args, **kwargs):
@@ -23,14 +20,3 @@ class SFTTrainer(SessionManagerMixIn, TRLSFTTrainer):
             return dataset
 
         return super()._prepare_dataset(dataset, *args, **kwargs)
-
-
-class SFTConfig(TrainingArguments, TRLSFTConfig):
-    """
-    This class is needed to wrap the llmcompressor.transformers.TrainingArguments
-    and TRLSFTConfig classes. This allows for the use of arguments and
-    configurations from both classes when training a model.
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)

src/llmcompressor/transformers/finetune/session_mixin.py
@@ -66,8 +66,8 @@ class SessionManagerMixIn:
     def __init__(
         self,
         recipe: str,
-        data_args: "DatasetArguments",
         model_args: "ModelArguments",
+        data_args: Optional["DatasetArguments"] = None,
         teacher: Optional[Union[Module, str]] = None,
         recipe_args: Optional[Union[Dict[str, Any], str]] = None,
         **kwargs,