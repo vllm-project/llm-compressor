e0cd917d31beb0a309f5798e62a5d389d612a5b2
Brian Dellabetta <bdellabe@redhat.com>
2025-02-18 20:32:53 +0000

    updated to be compatible with latest, unit tests passing
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/llmcompressor/transformers/finetune/data/pile.py
+++ src/llmcompressor/transformers/finetune/data/pile.py
@@ -1,7 +1,11 @@
 from copy import deepcopy
-from typing import Optional
+from typing import TYPE_CHECKING
 
 from llmcompressor.transformers.finetune.data import TextGenerationDataset
+from llmcompressor.typing import Processor
+
+if TYPE_CHECKING:
+    from llmcompressor.args import DatasetArguments
 
 
 @TextGenerationDataset.register(name="pile_eval")
@@ -13,33 +17,15 @@ class PileEvalDataset(TextGenerationDataset):
     :param tokenizer: tokenizer to use on dataset
     """
 
-    def __init__(self, data_args, split, tokenizer):
+    def __init__(self, data_args: "DatasetArguments", split: str, processor: Processor):
         data_args = deepcopy(data_args)
+        data_args.text_column = "text"
         data_args.dataset = "mit-han-lab/pile-val-backup"
-        super().__init__(
-            text_column="text", data_args=data_args, split=split, tokenizer=tokenizer
-        )
+        super().__init__(data_args=data_args, split=split, processor=processor)
 
-    def get_raw_dataset(self, cache_dir: Optional[str] = None):
-        """
-        Load the raw dataset from Hugging Face, using cached copy if available.
-        Additionally reformats the entries to fit the template.
-        :param cache_dir: disk location to search for cached dataset
-        :return: the requested dataset
-        """
-        raw_dataset = super().get_raw_dataset(cache_dir=cache_dir)
-
-        def restructure_fn(sample):
-            sample["text"] = sample["text"].strip()
-            return sample
-
-        raw_dataset = self.map(
-            raw_dataset,
-            function=restructure_fn,
-            batched=False,
-            remove_columns=["meta"],
-            num_proc=self.data_args.preprocessing_num_workers,
-            load_from_cache_file=not self.data_args.overwrite_cache,
-            desc="Restructuring Pile Dataset",
-        )
-        return raw_dataset
+    def dataset_template(self, sample):
+        return {
+            "text": self.processor.apply_chat_template(
+                sample["text"].strip(),
+            ),
+        }
