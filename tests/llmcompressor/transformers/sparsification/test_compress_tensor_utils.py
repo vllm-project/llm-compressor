70aecd5e03a59d1563dffcd89c5907248498905e
Kyle Sayers <kylesayrs@gmail.com>
2025-03-11 21:59:59 -0400

    increase max quantization error
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/transformers/sparsification/test_compress_tensor_utils.py
+++ tests/llmcompressor/transformers/sparsification/test_compress_tensor_utils.py
@@ -440,8 +440,8 @@ def test_compressor_stacking(model_stub, recipe, sparse_format, quant_format, tm
         if key.endswith("weight") and quant_format != "dense":
             # we don't expect an exact match for compressed
             diff = torch.abs(dense_tensor - reconstructed_tensor)
-            # max diff value found empirically
-            assert not torch.any(diff > 0.022), f"Max diff: {torch.max(diff)}"
+            # maximum quantization error as a result of compression is about 0.25
+            assert not torch.any(diff > 0.25), f"Max diff: {torch.max(diff)}"
         else:
             assert torch.equal(dense_tensor, reconstructed_tensor)
     shutil.rmtree(tmp_path)
