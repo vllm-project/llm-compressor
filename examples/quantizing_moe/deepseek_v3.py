cd50e63c80302873da3c95bb7e77c5545b065cb5
Kyle Sayers <kylesayrs@gmail.com>
2025-03-17 12:09:17 -0400

    del qconfig
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- examples/quantizing_moe/deepseek_v3.py
+++ examples/quantizing_moe/deepseek_v3.py
@@ -15,7 +15,7 @@ MODEL_ID = "deepseek-ai/DeepSeek-V3"
 #MODEL_ID = "deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct"
 #MODEL_ID = "deepseek-ai/DeepSeek-V2.5-1210"
 config = AutoConfig.from_pretrained(MODEL_ID, trust_remote_code=True)
-#del config.quantization_config
+del config.quantization_config
 
 # adjust based off number of desired GPUs
 # if not enough memory is available, some layers will automatically be offlaoded to cpu
