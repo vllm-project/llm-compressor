c6f28fb7933d819174e5db75f8bc379d21d1739d
Kyle Sayers <kylesayrs@gmail.com>
2025-03-17 11:03:44 -0400

    [Bugfix] Disable generation of deepseek models with transformers>=4.48 (#1259)
    ## Purpose ##
    * Generation is broken on deepseek models due to a deprecation by
    transformers. This PR skips generation in that case
    
    ## Related Issues ##
    * https://github.com/huggingface/transformers/issues/36071
    * https://huggingface.co/deepseek-ai/DeepSeek-V3/discussions/88
    
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- examples/quantizing_moe/deepseek_moe_w8a8_int8.py
+++ examples/quantizing_moe/deepseek_moe_w8a8_int8.py
@@ -1,6 +1,7 @@
 import torch
 from datasets import load_dataset
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from packaging.version import Version
+from transformers import AutoModelForCausalLM, AutoTokenizer, __version__
 
 from llmcompressor import oneshot
 from llmcompressor.modifiers.quantization import GPTQModifier
@@ -90,10 +91,19 @@ oneshot(
     output_dir=SAVE_DIR,
 )
 
-print("========== SAMPLE GENERATION ==============")
-SAMPLE_INPUT = ["I love quantization because"]
-tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
-inputs = tokenizer(SAMPLE_INPUT, return_tensors="pt", padding=True).to(model.device)
-output = model.generate(**inputs, max_length=50)
-text_output = tokenizer.batch_decode(output)
-print(text_output)
+# Confirm generations of the quantized model look sane.
+# Generation is broken for deepseek models when using the latest transformers package
+if Version(__version__) < Version("4.48"):
+    print("========== SAMPLE GENERATION ==============")
+    SAMPLE_INPUT = ["I love quantization because"]
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+    inputs = tokenizer(SAMPLE_INPUT, return_tensors="pt", padding=True).to(model.device)
+    output = model.generate(**inputs, max_length=50)
+    text_output = tokenizer.batch_decode(output)
+    print(text_output)
+    print("==========================================")
+else:
+    print(
+        "WARNING: cannot perform sample generation of "
+        "deepseek models with transformers >= 4.48"
+    )
