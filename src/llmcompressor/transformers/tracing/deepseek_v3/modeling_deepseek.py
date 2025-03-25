5b0f591f08587005131da5af79a39add3e2bdd53
Kyle Sayers <kylesayrs@gmail.com>
2025-03-25 13:35:05 -0400

    fix traceable generation
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/transformers/tracing/deepseek_v3/modeling_deepseek.py
+++ src/llmcompressor/transformers/tracing/deepseek_v3/modeling_deepseek.py
@@ -1678,7 +1678,7 @@ class DeepseekV3ForCausalLM(DeepseekV3PreTrainedModel):
             if isinstance(past_key_values, Cache):
                 cache_length = past_key_values.get_seq_length()
                 past_length = past_key_values.seen_tokens
-                max_cache_length = past_key_values.get_max_length()
+                max_cache_length = past_key_values.get_max_cache_shape()
             else:
                 cache_length = past_length = past_key_values[0][0].shape[2]
                 max_cache_length = None
