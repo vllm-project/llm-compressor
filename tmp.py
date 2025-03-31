30c71697fd764c29e4de774a14bc7a5c172d2102
Kyle Sayers <kylesayrs@gmail.com>
2025-03-31 14:43:15 -0400

    update script
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tmp.py
+++ tmp.py
@@ -3,13 +3,13 @@ from llmcompressor.modifiers.quantization.gptq import GPTQModifier
 from llmcompressor.modifiers.smoothquant.base import SmoothQuantModifier
 
 #model_id = "meta-llama/Llama-3.2-1B-instruct"
-model_id = "meta-llama/Llama-3.1-8B-Instruct"
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 recipe = [
-    #SmoothQuantModifier(smoothing_strength=0.8),
-    GPTQModifier(targets="Linear", scheme="W8A8", ignore=["lm_head"])
+    SmoothQuantModifier(smoothing_strength=0.8),
+    GPTQModifier(targets="Linear", scheme="FP8", ignore=["lm_head"])
 ]
-output_dir = model_id.split("/")[1] + "-W8A8"
+output_dir = model_id.split("/")[1] + "-FP8-independent"
 
 compressor = LLMCompressor(model_id, recipe)
 compressor.set_calibration_dataset("ultrachat_200k", split="train_sft[:512]")
-compressor.post_train(output_dir=output_dir)
+compressor.post_train(output_dir=output_dir, pipeline="independent")
