5a77b590e7bbe67ef9807e813baeb6370202e3ff
Kyle Sayers <kylesayrs@gmail.com>
2025-04-01 12:19:01 -0400

    Keep quantization enabled during calibration (#1299)
    ## Purpose ##
    * Revert the behavior regression introduced as a result of #1114
    * When calibrating a model using the `QuantizationModifier`,
    quantization should be enabled when calibrating
    
    ## Changes ##
    * Remove "disabling quantization" from the calibration forward pass
    * Add "disabling quantization" to the sequential pipelines in order to
    continue to disable quantization during calibration for GPTQ and SGPT
    * When [calibration pipelines become shared between modifiers](#1279),
    the decision of whether to disabling quantization during calibration
    will have to be moved to the calibration pipelines themselves. Some work
    needs to be done to demonstrate that GPTQ and SGPT do not suffer
    accuracy regression from enabling activation quantization during
    calibration (in theory, the change should increase accuracy)
    
    ---------
    
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/utils/helpers.py
+++ src/llmcompressor/utils/helpers.py
@@ -1013,7 +1013,7 @@ class DisableKVCache:
 @contextlib.contextmanager
 def DisableQuantization(module: torch.nn.Module):
     """
-    Disable quantization from QuantizationModifier
+    Disable quantization during forward passes after applying a quantization config
     """
     try:
         module.apply(disable_quantization)
@@ -1040,13 +1040,11 @@ def calibration_forward_context(model: PreTrainedModel):
 
     - Remove gradient calculations
     - Disable the KV cache
-    - Disable quantization during forward pass
     - Disable train mode and enable eval mode
     """
     with (
         torch.no_grad(),
         DisableKVCache(model),
-        DisableQuantization(model),
         eval_context(model),
     ):
         yield
