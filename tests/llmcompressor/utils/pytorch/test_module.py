5cfa4b16e3c9b3574bc7ba3837f04305b830cf23
Brian Dellabetta <bdellabe@redhat.com>
2025-04-10 21:51:45 +0000

    revise get_parent_by_name test
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- tests/llmcompressor/utils/pytorch/test_module.py
+++ tests/llmcompressor/utils/pytorch/test_module.py
@@ -1,31 +1,44 @@
-import unittest
+import pytest
 
 import torch.nn as nn
 
 from llmcompressor.utils.pytorch import get_parent_by_name
 
 
-class TestGetParentByName(unittest.TestCase):
-    def setUp(self):
-        self.model = nn.Sequential(
-            nn.Linear(10, 20), nn.ReLU(), nn.Linear(20, 10), nn.Softmax(dim=1)
-        )
+@pytest.fixture
+def example_nested_module() -> str:
+    return nn.Sequential(
+        nn.Linear(10, 20),
+        nn.Sequential(nn.ReLU(), nn.Linear(20, 10)),
+        nn.Sequential(nn.SiLU(), nn.Linear(20, 10)),
+        nn.Softmax(dim=1),
+    )
 
-    def test_get_parent_by_name(self):
-        # Test getting the parent of a non-existent layer
-        with self.assertRaises(ValueError):
-            get_parent_by_name("non_existent_layer", self.model)
 
-        # Test getting the parent of the first layer
-        name, parent = get_parent_by_name("0", self.model)
-        self.assertEqual(parent, self.model)
+@pytest.mark.unit
+def test_get_parent_by_name(example_nested_module):
 
-        # Test getting the parent of a nested layer
-        nested_model = nn.Sequential(
-            nn.Linear(10, 20),
-            nn.Sequential(nn.ReLU(), nn.Linear(20, 10)),
-            nn.Softmax(dim=1),
-        )
-        name, parent = get_parent_by_name("1.1", nested_model)
-        self.assertEqual(parent, nested_model[1])
-        self.assertEqual(name, "1")
+    # Test getting the parent of the first layer
+    name, parent = get_parent_by_name("0", example_nested_module)
+    assert parent == example_nested_module
+
+    # Test getting the parent of a nested layer
+    name, parent = get_parent_by_name("1.0", example_nested_module)
+    assert parent == example_nested_module[1]
+    assert name == "1"
+
+    name, parent = get_parent_by_name("1.1", example_nested_module)
+    assert parent == example_nested_module[1]
+    assert name == "1"
+
+    name, parent = get_parent_by_name("2.0", example_nested_module)
+    assert parent == example_nested_module[2]
+    assert name == "2"
+
+    name, parent = get_parent_by_name("2.1", example_nested_module)
+    assert parent == example_nested_module[2]
+    assert name == "2"
+
+    # Test getting the parent of a non-existent layer
+    with pytest.raises(ValueError):
+        get_parent_by_name("non_existent_layer", example_nested_module)
