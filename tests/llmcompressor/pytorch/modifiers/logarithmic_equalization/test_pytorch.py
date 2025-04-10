4a58b5c9d53f824b5c63eb68705eb7ff93bd99fa
Brian Dellabetta <bdellabe@redhat.com>
2025-04-10 22:34:12 +0000

    revert smoothquant changes
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- tests/llmcompressor/pytorch/modifiers/logarithmic_equalization/test_pytorch.py
+++ tests/llmcompressor/pytorch/modifiers/logarithmic_equalization/test_pytorch.py
@@ -21,11 +21,11 @@ class TestLogEqualizationMapping(unittest.TestCase):
         modifier = LogarithmicEqualizationModifier(mappings=mappings)
 
         modifier.ignore = []
-        modifier._resolved_mappings = modifier._resolve_mappings(self.state.model)
+        modifier.resolved_mappings_ = modifier._resolve_mappings(self.state.model)
 
-        self.assertEqual(len(modifier._resolved_mappings), len(mappings))
+        self.assertEqual(len(modifier.resolved_mappings_), len(mappings))
 
-        mapping = modifier._resolved_mappings[0]
+        mapping = modifier.resolved_mappings_[0]
         self.assertEqual(mapping.smooth_name, mappings[0][1])
         self.assertIsInstance(mapping.smooth_layer, Linear)
         self.assertIsInstance(mapping.balance_layers[0], Linear)
