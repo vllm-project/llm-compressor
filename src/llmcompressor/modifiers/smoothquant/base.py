03086dd5165742241acb8f74e8b28f365767b7e2
Kyle Sayers <kylesayrs@gmail.com>
2025-04-15 17:29:31 -0400

    style
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/modifiers/smoothquant/base.py
+++ src/llmcompressor/modifiers/smoothquant/base.py
@@ -4,7 +4,7 @@ from typing import Callable, Dict, List, Optional, Tuple, Union
 import torch
 from compressed_tensors.utils import align_module_device
 from loguru import logger
-from pydantic import Field, ConfigDict
+from pydantic import ConfigDict, Field
 from torch.nn import Module
 
 from llmcompressor.core import State
@@ -106,7 +106,9 @@ class SmoothQuantModifier(Modifier):
     num_calibration_steps: Optional[int] = None
     calibration_function: Optional[Callable] = None
 
-    resolved_mappings_: Optional[List[SmoothQuantMapping]] = Field(default=None, repr=False)
+    resolved_mappings_: Optional[List[SmoothQuantMapping]] = Field(
+        default=None, repr=False
+    )
     scales_: Optional[Dict] = Field(default=None, repr=False)
 
     def on_initialize(self, state: State, **kwargs) -> bool:
