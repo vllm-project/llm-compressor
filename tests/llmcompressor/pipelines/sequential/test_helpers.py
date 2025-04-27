fcca3ca685ef40b29c11840c167359c9b3ce8c00
Kyle Sayers <kylesayrs@gmail.com>
2025-04-27 14:57:36 -0400

    better syntax
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/pipelines/sequential/test_helpers.py
+++ tests/llmcompressor/pipelines/sequential/test_helpers.py
@@ -6,7 +6,7 @@ from llmcompressor.pipelines.sequential.helpers import get_sequential_ancestors
 class DummyModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.sq = torch.nn.Sequential(torch.nn.Linear(10, 20), torch.nn.ReLU())
+        self.seq = torch.nn.Sequential(torch.nn.Linear(10, 20), torch.nn.ReLU())
         self.fc = torch.nn.Linear(20, 5)
 
     def forward(self, x):
@@ -18,7 +18,7 @@ def test_get_sequential_ancestors():
     model = DummyModel()
 
     assert get_sequential_ancestors(model, set()) == set()
-    assert get_sequential_ancestors(model, set([model])) == set()
-    assert get_sequential_ancestors(model, set([model.fc])) == set([model])
-    assert get_sequential_ancestors(model, set([model.sq[0]])) == set([model, model.sq])
-    assert get_sequential_ancestors(model, set([model.sq[1]])) == set([model, model.sq])
+    assert get_sequential_ancestors(model, {model}) == set()
+    assert get_sequential_ancestors(model, {model.fc}) == {model}
+    assert get_sequential_ancestors(model, {model.seq[0]}) == {model, model.seq}
+    assert get_sequential_ancestors(model, {model.seq[1]}) == {model, model.seq}
