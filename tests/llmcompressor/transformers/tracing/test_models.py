8c75c0dd3e2c402f9db2086b11e547611c65a6c0
Kyle Sayers <kylesayrs@gmail.com>
2025-04-29 18:09:19 -0400

    cleanup, do not require ignore
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/transformers/tracing/test_models.py
+++ tests/llmcompressor/transformers/tracing/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, WhisperForConditionalGeneration
 
 from llmcompressor.transformers.tracing import (
     TraceableIdefics3ForConditionalGeneration,
@@ -32,40 +32,41 @@ def test_text_trace(model_id, model_class, targets):
     "model_id,model_class,targets,ignore",
     [
         (
-            "Qwen/Qwen2-VL-2B-Instruct",
-            TraceableQwen2VLForConditionalGeneration,
-            None,
-            ["lm_head", "re:visual.*"],
-        ),
-        (
-            "Qwen/Qwen2.5-VL-7B-Instruct",
-            TraceableQwen2_5_VLForConditionalGeneration,
-            None,
-            ["lm_head", "re:visual.*"],
-        ),
-        (
-            "mgoin/pixtral-12b",
-            TraceableLlavaForConditionalGeneration,
-            ["MistralDecoderLayer"],
-            ["re:.*lm_head", "re:vision_tower.*", "re:multi_modal_projector.*"],
-        ),
-        (
-            "meta-llama/Llama-3.2-11B-Vision-Instruct",
-            TraceableMllamaForConditionalGeneration,
-            None,
-            ["re:.*lm_head", "re:multi_modal_projector.*", "re:vision_model.*"],
+            "HuggingFaceM4/Idefics3-8B-Llama3",
+            TraceableIdefics3ForConditionalGeneration,
+            ["LlamaDecoderLayer"],
+            [],
         ),
         (
             "llava-hf/llava-1.5-7b-hf",
             TraceableLlavaForConditionalGeneration,
             ["LlamaDecoderLayer"],
-            ["re:.*lm_head", "re:vision_tower.*", "re:multi_modal_projector.*"],
+            [],
         ),
         (
-            "HuggingFaceM4/Idefics3-8B-Llama3",
-            TraceableIdefics3ForConditionalGeneration,
-            ["Idefics3EncoderLayer", "LlamaDecoderLayer"],
-            ["re:.*lm_head", "re:model.vision_model.*", "re:model.connector.*"],
+            "meta-llama/Llama-3.2-11B-Vision-Instruct",
+            TraceableMllamaForConditionalGeneration,
+            ["MllamaSelfAttentionDecoderLayer"],
+            [],
+        ),
+        # skip phi3_v because of its processor is annoying and requires special code
+        (
+            "mgoin/pixtral-12b",
+            TraceableLlavaForConditionalGeneration,
+            ["MistralDecoderLayer"],
+            [],
+        ),
+        (
+            "Qwen/Qwen2.5-VL-7B-Instruct",
+            TraceableQwen2_5_VLForConditionalGeneration,
+            ["Qwen2_5_VLDecoderLayer"],
+            [],
+        ),
+        (
+            "Qwen/Qwen2-VL-2B-Instruct",
+            TraceableQwen2VLForConditionalGeneration,
+            ["Qwen2VLDecoderLayer"],
+            [],
         ),
     ],
 )
@@ -76,5 +77,27 @@ def test_vision_trace(model_id, model_class, targets, ignore):
         targets,
         ignore=ignore,
         modality="vision",
+        trust_remote_code=True,
+    )
+
+
+@pytest.mark.parametrize(
+    "model_id,model_class,targets,ignore",
+    [
+        (
+            "openai/whisper-large-v3",
+            WhisperForConditionalGeneration,
+            None,
+            [],
+        ),
+    ],
+)
+def test_audio_trace(model_id, model_class, targets, ignore):
+    trace(
+        model_id,
+        model_class,
+        targets,
+        ignore=ignore,
+        modality="audio",
         trust_remote_code=True,
     )
