711ff8ee859dbdd693f37b5db142647ddfe1a21e
Brian Dellabetta <bdellabe@redhat.com>
2025-05-14 14:29:41 +0000

    Cleanup for ready PR
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/llmcompressor/modifiers/quantization/quantization/mixin.py
+++ src/llmcompressor/modifiers/quantization/quantization/mixin.py
@@ -53,7 +53,7 @@ class QuantizationMixin(HooksMixin):
         NOTE: QuantizationMixin does not update scales and zero-points on its own,
           as this is not desired for all Modifiers inheriting from it. Modifier must
           explicitly call `update_weight_zp_scale`.
-          See QuantizationMixin.on_start method for example
+          See QuantizationModifier.on_start method for example
 
     :param config_groups: dictionary specifying quantization schemes to apply to target
         modules. Modules not matching a scheme target will NOT be quantized.
