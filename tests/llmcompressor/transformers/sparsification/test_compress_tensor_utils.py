b30198321c3bb5e7f45ff50b5793163b24054e78
Kyle Sayers <kylesayrs@gmail.com>
2025-05-19 16:59:57 -0400

    use proper offloading
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/transformers/sparsification/test_compress_tensor_utils.py
+++ tests/llmcompressor/transformers/sparsification/test_compress_tensor_utils.py
@@ -14,7 +14,7 @@ from compressed_tensors.quantization import (
     QuantizationStatus,
     quantize,
 )
-from compressed_tensors.utils import get_offloaded_device, update_prefix_dict
+from compressed_tensors.utils import align_module_device, update_offload_parameter
 from torch import nn
 from transformers import AutoConfig, AutoModelForCausalLM
 from transformers.utils.quantization_config import CompressedTensorsConfig
@@ -318,16 +318,8 @@ def test_model_shared_tensors(
         model = model.to(device)
 
     # modify lm head
-    with torch.no_grad():
-        if offload:
-            model.lm_head._hf_hook.pre_forward(model.lm_head)
-
-        model.lm_head.weight += 1
-
-        if offload:
-            device = get_offloaded_device(model.lm_head)
-            update_prefix_dict(model.lm_head, "weight", model.lm_head.weight.to(device))
-            model.lm_head._hf_hook.post_forward(model.lm_head, None)
+    with torch.no_grad(), align_module_device(model.lm_head):
+        update_offload_parameter(model.lm_head, "weight", model.lm_head.weight + 1)
 
     # check that embed_tokens is not modified
     model_dict = get_state_dict_offloaded_model(model)
