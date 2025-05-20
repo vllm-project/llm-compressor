7769942980f834f081906989a9b5489d181b718f
Kyle Sayers <kylesayrs@gmail.com>
2025-05-20 16:56:05 -0400

    fix resolution, add sentinel
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

---
+++ src/llmcompressor/sentinel.py
@@ -0,0 +1,41 @@
+import inspect
+
+_registry = {}
+
+
+class Sentinel:
+    """
+    Unique sentinel values. Implements https://peps.python.org/pep-0661/
+    """
+
+    def __new__(cls, name, module_name=None):
+        name = str(name)
+
+        if module_name is None:
+            module_name = inspect.currentframe().f_globals.get("__file__")
+            if module_name is None:
+                module_name = __name__
+
+        registry_key = f"{module_name}-{name}"
+
+        sentinel = _registry.get(registry_key, None)
+        if sentinel is not None:
+            return sentinel
+
+        sentinel = super().__new__(cls)
+        sentinel._name = name
+        sentinel._module_name = module_name
+
+        return _registry.setdefault(registry_key, sentinel)
+
+    def __repr__(self):
+        return self._name
+
+    def __reduce__(self):
+        return (
+            self.__class__,
+            (
+                self._name,
+                self._module_name,
+            ),
+        )
