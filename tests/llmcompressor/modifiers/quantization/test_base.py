8d7fb4eb35d1963575495ca22340ab7d32a0c5f7
Kyle Sayers <kylesayrs@gmail.com>
2025-05-29 14:16:20 -0400

    add tests
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/modifiers/quantization/test_base.py
+++ tests/llmcompressor/modifiers/quantization/test_base.py
@@ -1,70 +1,76 @@
+from contextlib import nullcontext
+
 import pytest
-from compressed_tensors.quantization import ActivationOrdering
 
 from llmcompressor.modifiers.quantization import GPTQModifier
 
 
 @pytest.fixture
-def q_config_kwargs(recipe_actorder_0, recipe_actorder_1):
-    return {
-        "config_groups": {
-            "group_0": {
-                "targets": ["Linear"],
-                "input_activations": {
-                    "num_bits": 8,
-                    "symmetric": False,
-                    "strategy": "token",
-                    "dynamic": "true",
-                    "kwargs": {},
-                },
-                "weights": {
-                    "num_bits": 4,
-                    "symmetric": True,
-                    "strategy": "channel",
-                    "kwargs": {},
-                    "actorder": recipe_actorder_0,
-                },
-            },
-            "group_1": {
-                "targets": ["Linear"],
-                "input_activations": {
-                    "num_bits": 8,
-                    "symmetric": False,
-                    "strategy": "token",
-                    "dynamic": "true",
-                    "kwargs": {},
-                },
-                "weights": {
-                    "num_bits": 4,
-                    "symmetric": True,
-                    "strategy": "channel",
-                    "kwargs": {},
-                    "actorder": recipe_actorder_1,
-                },
-            },
-        },
-    }
+def q_config_kwargs(config_0, config_1):
+    return dict(
+        config_groups=dict(
+            group_0=dict(
+                targets=["Linear"],
+                input_activations=dict(num_bits=8, symmetric=False, strategy="token"),
+                weights=dict(
+                    num_bits=4,
+                    symmetric=True,
+                    strategy="group",
+                    group_size=128,
+                    actorder=config_0,
+                ),
+            ),
+            group_1=dict(
+                targets=["Linear"],
+                input_activations=dict(num_bits=8, symmetric=False, strategy="token"),
+                weights=dict(
+                    num_bits=4,
+                    symmetric=True,
+                    strategy="group",
+                    group_size=128,
+                    actorder=config_1,
+                ),
+            ),
+        )
+    )
 
 
 @pytest.mark.parametrize(
-    "has_actorder,actorder,recipe_actorder_0,recipe_actorder_1,",
+    "has_actorder,actorder,config_0,config_1,expected_0,expected_1",
     [
-        (True, "group", None),
-        (True, "static"),
-        (True, None),
-        (True, ActivationOrdering.DYNAMIC),
-        (True, ActivationOrdering.WEIGHT),
-        (False, None),
+        # defaults to None if nothing provided
+        (False, None, None, None, None, None),
+        # modifier overrides config if no config provided
+        (True, "group", None, None, "group", "group"),
+        # modifier overrides if config partially matches anyways
+        (True, "group", None, "group", "group", "group"),
+        (True, "group", "group", None, "group", "group"),
+        # modifier errors if conflict with config
+        (True, "group", None, "static", "error", "error"),
+        (True, "group", "static", None, "error", "error"),
+        # modifier does not override if not provided
+        (False, "N/A", None, None, None, None),
+        (False, "N/A", None, "static", None, "static"),
+        (False, "N/A", "static", None, "static", None),
+        (False, "N/A", "static", "static", "static", "static"),
+        (False, "N/A", None, "group", None, "group"),
+        (False, "N/A", "group", None, "group", None),
+        (False, "N/A", "group", "group", "group", "group"),
     ],
 )
 def test_actorder_resolution(
-    has_actorder, actorder, recipe_actorder_0, recipe_actorder_1, q_config_kwargs
+    has_actorder, actorder, q_config_kwargs, expected_0, expected_1
 ):
     if has_actorder:
         modifier = GPTQModifier(**q_config_kwargs, actorder=actorder)
     else:
         modifier = GPTQModifier(**q_config_kwargs)
 
-    config = modifier.resolve_quantization_config()
-    assert config.config_groups["group_0"].input_activations.actorder is None
-    assert config.config_groups["group_0"].weights.actorder == actorder
+    with pytest.raises(ValueError) if expected_0 == "error" else nullcontext():
+        resolved = modifier.resolve_quantization_config()
+
+    if expected_0 != "error":
+        assert resolved.config_groups["group_0"].input_activations.actorder is None
+        assert resolved.config_groups["group_0"].weights.actorder == expected_0
+        assert resolved.config_groups["group_1"].input_activations.actorder is None
+        assert resolved.config_groups["group_1"].weights.actorder == expected_1
