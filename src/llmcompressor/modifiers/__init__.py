8680d9578d12f1e5e9d25e4b867d2f8b0b668d86
shanjiaz <43143795+shanjiaz@users.noreply.github.com>
2025-06-27 12:39:23 -0400

    Removed recipeStage, recipeModifer, stageModifier classes  (#1514)
    SUMMARY:
    Removed recipeStage, recipeModifer, stageModifier classes and
    dependencies.
    
    
    TEST PLAN:
    Tested locally
    
    DETAILS & CONCERNS:
    1. `recipe.py` is where most changes happened. 
    - Rewrote the parsing logic in `parse_from_dict`. 
    - `Recipe` class now is basically just a list of `Modifiers`, all other
    changes are downstream dependencies.
    - All the serilization helpers and deserialization helpers now live in
    `utils`. Had to rewrite serialization logic.
    - Stage is stored as a string by `self.stage`.
    2. Had to update a test in `test_recipe`. We support creating a recipe
    instance by reading a multi-stage recipe but no longer support
    serializing multi-stage recipes directly. The current structure assumes
    one recipe could only have one stage.
    3. Had to rewrite `update_and_save_recipe` function since now each
    recipe only has one stage and therefore can't handle merging multi-stage
    recipes. The new setup:
    - `infer_recipe_from_model_path` will find existing recipe path. 
    - `yaml` method now takes in an optional argument `existing_recipe_path`
    and merge the existing recipe with the current recipe. Since there would
    only be at most one existing recipe at a time, we should be safe.
    
    TODO:
    - [x]  Add validations for recipe class. 
    - [x] Simplify `recipe.py` structure
    
    ---------
    
    Signed-off-by: shanjiaz <zsjwpianpian@gmail.com>
    Co-authored-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/modifiers/__init__.py
+++ src/llmcompressor/modifiers/__init__.py
@@ -1,11 +1,9 @@
 from .factory import ModifierFactory
 from .interface import ModifierInterface
 from .modifier import Modifier
-from .stage import StageModifiers
 
 __all__ = [
     "ModifierFactory",
     "ModifierInterface",
     "Modifier",
-    "StageModifiers",
 ]
--- src/llmcompressor/pipelines/sequential/pipeline.py
+++ src/llmcompressor/pipelines/sequential/pipeline.py
@@ -57,8 +57,9 @@ class SequentialPipeline(CalibrationPipeline):
         model_device = get_execution_device(model)
 
         # prepare to trace subgraphs
-        modifiers = session.get_modifiers()
+        modifiers = session.lifecycle.recipe.modifiers
         sequential_targets = get_sequential_targets(modifiers, model, dataset_args)
+
         ignore = dataset_args.tracing_ignore
 
         # trace subgraphs
