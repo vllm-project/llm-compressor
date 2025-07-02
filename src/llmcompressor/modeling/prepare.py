c133210f6d4fc25f28e04e45eb285984aa4a269c
Dipika <dipikasikka1@gmail.com>
2025-07-02 14:32:21 -0400

    update

--- src/llmcompressor/modeling/prepare.py
+++ src/llmcompressor/modeling/prepare.py
@@ -2,6 +2,7 @@ from compressed_tensors.utils import replace_module
 from transformers import PreTrainedModel
 
 from llmcompressor.modeling.deepseek_v3 import replace as replace_DeepseekV3MoE
+from llmcompressor.modeling.qwen3_moe import replace as replace_Qwen3MoE
 from llmcompressor.utils.helpers import patch_attr
 
 __all__ = ["prepare_for_calibration"]
@@ -22,25 +23,15 @@ def prepare_for_calibration(model: PreTrainedModel) -> PreTrainedModel:
 
 
 def update_qwen3_moe(model, stack):
-    for module in model.model.layers:
-        stack.enter_context(patch_attr(module.mlp, "top_k", model.config.num_experts))
-
-
-def update_deepseekv3(model, stack):
-    for i in range(len(model.model.layers)):
-        if i > 2:
-            stack.enter_context(
-                patch_attr(
-                    model.model.layers[i],
-                    "mlp",
-                    replace_DeepseekV3MoE(model.model.layers[i].mlp),
-                )
-            )
+    for _, module in model.named_modules():
+        cls_name = module.__class__.__name__
+        print(cls_name)
+        if cls_name == "Qwen3MoeDecoderLayer":
+            stack.enter_context(patch_attr(module, "mlp", replace_Qwen3MoE()))
 
 
 moe_context = {
     "Qwen3MoeForCausalLM": update_qwen3_moe,
-    "DeepseekV3ForCausalLM": update_deepseekv3,
 }
 
 
