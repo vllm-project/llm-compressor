6f9aaeb4524a04bc3cd64aa6b5757b4b1914ae99
Kyle Sayers <kylesayrs@gmail.com>
2025-07-14 13:57:08 -0400

    initialize weight with random values
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/modeling/test_fuse.py
+++ tests/llmcompressor/modeling/test_fuse.py
@@ -17,16 +17,16 @@ def test_center_embeddings():
 @pytest.mark.unit
 def test_fuse_norm_linears():
     norm = torch.nn.LayerNorm((5,))
+    norm.weight.data = torch.rand(norm.weight.shape)
     linears = [
         torch.nn.Linear(5, 5),
         torch.nn.Linear(5, 5),
     ]
-    input = torch.rand((1, 5), requires_grad=False)
 
+    input = torch.rand((1, 5), requires_grad=False)
     true_output = torch.stack([linear(norm(input)) for linear in linears])
 
     fuse_norm_linears(norm, linears)
-
     output = torch.stack([linear(norm(input)) for linear in linears])
 
     assert torch.allclose(true_output, output)
