29c20f90ee172168bf53879c8049a37bba071d9f
Kyle Sayers <kylesayrs@gmail.com>
2025-07-14 10:55:10 -0400

    add precision and validation
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/modeling/fuse.py
+++ src/llmcompressor/modeling/fuse.py
@@ -1,9 +1,29 @@
 from typing import Iterable
 
 import torch
-from compressed_tensors import update_offload_parameter
+from compressed_tensors import (
+    align_module_device,
+    get_execution_device,
+    update_offload_parameter,
+)
 
-__all__ = ["fuse_norm_linears"]
+__all__ = ["normalize_embedding", "fuse_norm_linears"]
+
+
+PRECISION = torch.float64
+
+
+def normalize_embedding(embedding: torch.nn.Module):
+    if not hasattr(embedding, "weight"):
+        raise ValueError(f"Cannot fuse norm of type {type(embedding)}")
+
+    with align_module_device(embedding):
+        weight_dtype = embedding.weight.dtype
+        weight = embedding.weight.to(PRECISION)
+        new_weight = weight - weight.mean(dim=-1, keepdim=True)
+        new_weight = new_weight.to(weight_dtype)
+
+    update_offload_parameter(embedding, "weight", new_weight)
 
 
 def fuse_norm_linears(norm: torch.nn.Module, linears: Iterable[torch.nn.Linear]):
@@ -16,13 +36,19 @@ def fuse_norm_linears(norm: torch.nn.Module, linears: Iterable[torch.nn.Linear])
     :param norm: norm layer whose weight will be fused into subsequent linears
     :param linears: linear layers which directly follow the norm layer
     """
-    if isinstance(norm, torch.nn.RMSNorm):
-        for linear in linears:
-            # spinquant does this op in float64
-            new_weight = linear.weight * norm.weight
-            update_offload_parameter(linear, "weight", new_weight)
-
-        update_offload_parameter(norm, "weight", torch.ones_like(norm.weight))
-
-    else:
+    if not hasattr(norm, "weight"):
         raise ValueError(f"Cannot fuse norm of type {type(norm)}")
+
+    for linear in linears:
+        # NOTE: spinquant does this op in float64
+        exec_device = get_execution_device(norm)
+        with align_module_device(norm, exec_device), align_module_device(
+            linear, exec_device
+        ):
+            weight_dtype = linear.weight.dtype
+            new_weight = linear.weight.to(PRECISION) * norm.weight.to(PRECISION)
+            new_weight = new_weight.to(weight_dtype)
+
+        update_offload_parameter(linear, "weight", new_weight)
+
+    update_offload_parameter(norm, "weight", torch.ones_like(norm.weight))
