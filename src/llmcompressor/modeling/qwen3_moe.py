4e98381c72dbe86ad474e6900f08bf77b787af17
Dipika Sikka <dipikasikka1@gmail.com>
2025-07-15 02:06:14 +0000

    update

--- src/llmcompressor/modeling/qwen3_moe.py
+++ src/llmcompressor/modeling/qwen3_moe.py
@@ -27,7 +27,7 @@ class Qwen3MoeSparseMoeBlock(torch.nn.Module):
     ):
         super().__init__()
         self.num_experts = config.num_experts
-        self.top_k = config.num_experts
+        self.top_k = config.top_k
         self.norm_topk_prob = config.norm_topk_prob
 
         # gating
@@ -62,6 +62,7 @@ class Qwen3MoeSparseMoeBlock(torch.nn.Module):
             selected_experts, num_classes=self.num_experts
         ).permute(2, 1, 0)
 
+        """
         expert_hitted = torch.greater(expert_mask.sum(dim=(-1, -2)), 0).nonzero()
         for expert_idx in expert_hitted:
             expert_layer = self.experts[expert_idx]
@@ -75,6 +76,21 @@ class Qwen3MoeSparseMoeBlock(torch.nn.Module):
                 expert_layer(current_state) * routing_weights[top_x, idx, None]
             )
 
+            # However `index_add_` only support torch tensors for indexing so we'll use
+            # the `top_x` tensor here.
+            final_hidden_states.index_add_(
+                0, top_x, current_hidden_states.to(hidden_states.dtype)
+            )
+        """
+        for expert_idx in range(len(self.experts)):
+            expert_layer = self.experts[expert_idx]
+            idx, top_x = torch.where(expert_mask[expert_idx].squeeze(0))
+            # Index the correct hidden states and compute the expert hidden state for
+            # the current expert. We need to make sure to multiply the output hidden
+            # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+            current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
+            expert_output = expert_layer(current_state)
+            current_hidden_states = expert_output * routing_weights[top_x, idx, None]
             # However `index_add_` only support torch tensors for indexing so we'll use
             # the `top_x` tensor here.
             final_hidden_states.index_add_(
