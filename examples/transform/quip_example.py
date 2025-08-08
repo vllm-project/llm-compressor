7d34cca59c234cf9a5951bac24bb84ddefdae614
Kyle Sayers <kylesayrs@gmail.com>
2025-08-08 00:53:47 -0400

    remove calib dataset, add note
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- examples/transform/quip_example.py
+++ examples/transform/quip_example.py
@@ -1,9 +1,8 @@
 """
 NOTE: Models produced by this example will not be runnable in vLLM without
-the following changes: https://github.com/vllm-project/vllm/pull/22219
+the following changes: https://github.com/vllm-project/vllm/pull/22486
 """
 
-from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from llmcompressor import oneshot
@@ -12,52 +11,11 @@ from llmcompressor.modifiers.transform import QuIPModifier
 from llmcompressor.utils import dispatch_for_generation
 
 # Select model and load it.
+# NOTE: because the datafree pipeline is being used in this
+# example, you can use additional GPUs to support larger models
 MODEL_ID = "meta-llama/Llama-3.1-8B-Instruct"
-
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL_ID,
-    torch_dtype="auto",
-)
+model = AutoModelForCausalLM.from_pretrained(MODEL_ID, torch_dtype="auto")
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
-
-# Select calibration dataset.
-DATASET_ID = "HuggingFaceH4/ultrachat_200k"
-DATASET_SPLIT = "train_sft"
-
-# Select number of samples. 512 samples is a good place to start.
-# Increasing the number of samples can improve accuracy.
-NUM_CALIBRATION_SAMPLES = 512
-MAX_SEQUENCE_LENGTH = 2048
-
-# Load dataset and preprocess.
-ds = load_dataset(DATASET_ID, split=f"{DATASET_SPLIT}[:{NUM_CALIBRATION_SAMPLES}]")
-ds = ds.shuffle(seed=42)
-
-
-def preprocess(example):
-    return {
-        "text": tokenizer.apply_chat_template(
-            example["messages"],
-            tokenize=False,
-        )
-    }
-
-
-ds = ds.map(preprocess)
-
-
-# Tokenize inputs.
-def tokenize(sample):
-    return tokenizer(
-        sample["text"],
-        padding=False,
-        max_length=MAX_SEQUENCE_LENGTH,
-        truncation=True,
-        add_special_tokens=False,
-    )
-
-
-ds = ds.map(tokenize, remove_columns=ds.column_names)
 
 # Configure the quantization algorithm to run.
 #   * apply spinquant transforms to model in order to make quantization easier
@@ -68,14 +26,7 @@ recipe = [
 ]
 
 # Apply algorithms.
-oneshot(
-    model=model,
-    recipe=recipe,
-    dataset=ds,
-    max_seq_length=MAX_SEQUENCE_LENGTH,
-    num_calibration_samples=NUM_CALIBRATION_SAMPLES,
-    pipeline="datafree",
-)
+oneshot(model=model, recipe=recipe, pipeline="datafree")
 
 # Confirm generations of the quantized model look sane.
 print("\n\n")
