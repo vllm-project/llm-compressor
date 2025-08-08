7324f4bec1ad8bf13402d16201b530279a98854c
Kyle Sayers <kylesayrs@gmail.com>
2025-08-08 00:36:48 -0400

    move tests files
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/modifiers/transform/quip/test_serialization.py
+++
@@ -1,7 +0,0 @@
-from llmcompressor.modifiers.transform import QuIPModifier
-
-
-def test_reload():
-    modifier = QuIPModifier(transform_type="hadamard")
-    dump = modifier.model_dump()
-    assert QuIPModifier.model_validate(dump) == modifier
