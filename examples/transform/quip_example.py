64151314fec6d946b20edc15880a038148de207f
Kyle Sayers <kylesayrs@gmail.com>
2025-08-14 16:20:17 -0400

    [Transform] QuIP Modifier (#1648)
    ## Purpose ##
    * Enable quip-style transforms
    
    ## Prerequisites ##
    * https://github.com/neuralmagic/compressed-tensors/pull/370
    * https://github.com/neuralmagic/compressed-tensors/pull/412
    * https://github.com/neuralmagic/compressed-tensors/pull/414
    
    ## Changes ##
    * Added `quip_example.py` to examples folder
    * As made clear in the disclaimer, this example requires minimum
    versions of compressed-tensors and transformers to run
    * Added `QuIPModifier` which handles the construction of a quip-style
    transform config
    
    ## Testing ##
    * Added modifier serialization and correctness tests
    
    ## Evaluation ##
    Evaluation performed by @brian-dellabetta 
    
    Evals on Llama 3.2 1B with Quip (num_fewshot 8, limit 1000 to be
    compatible with results
    [here](https://github.com/vllm-project/llm-compressor/pull/1243/files#diff-bdc27f23c0dc2da352d5c83abdc0f267873edf4d36f88474038b975df75bd8c3R38-R64))
    :
    
    | Strat | gsm8k,strict | gsm8k_llama,strict |
    |-|-|-|
    | FP16 | .352 | .323 |
    | Quip | .348 | .322 |
    | W4A16 | .180 | .017 |
    | Quip+W4A16 | .213 | .141 |
    
    ## Follow Ups ##
    * Infer data free pipeline, even if a transform modifier is included
    * Modify example to use GPTQ once basic evaluation has been performed
    
    ---------
    
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>
    Co-authored-by: Brian Dellabetta <bdellabe@redhat.com>
    Co-authored-by: Dipika Sikka <dipikasikka1@gmail.com>

---
+++ examples/transform/quip_example.py
@@ -0,0 +1,43 @@
+"""
+NOTE: Models produced by this example will not be runnable in vLLM without
+the following changes: https://github.com/vllm-project/vllm/pull/22486
+"""
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from llmcompressor import oneshot
+from llmcompressor.modifiers.quantization import QuantizationModifier
+from llmcompressor.modifiers.transform import QuIPModifier
+from llmcompressor.utils import dispatch_for_generation
+
+# Select model and load it.
+# NOTE: because the datafree pipeline is being used in this
+# example, you can use additional GPUs to support larger models
+MODEL_ID = "meta-llama/Llama-3.1-8B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(MODEL_ID, torch_dtype="auto")
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+
+# Configure the quantization algorithm to run.
+#   * apply spinquant transforms to model in order to make quantization easier
+#   * quantize the weights to 4 bit with a group size 128
+recipe = [
+    QuIPModifier(transform_type="random-hadamard"),
+    QuantizationModifier(targets="Linear", scheme="W4A16", ignore=["lm_head"]),
+]
+
+# Apply algorithms.
+oneshot(model=model, recipe=recipe, pipeline="datafree")
+
+# Confirm generations of the quantized model look sane.
+print("\n\n")
+print("========== SAMPLE GENERATION ==============")
+dispatch_for_generation(model)
+input_ids = tokenizer("Hello my name is", return_tensors="pt").input_ids.to("cuda")
+output = model.generate(input_ids, max_new_tokens=100)
+print(tokenizer.decode(output[0]))
+print("==========================================\n\n")
+
+# Save to disk compressed.
+SAVE_DIR = MODEL_ID.split("/")[1] + "-quip-w4a16"
+model.save_pretrained(SAVE_DIR, save_compressed=True)
+tokenizer.save_pretrained(SAVE_DIR)
