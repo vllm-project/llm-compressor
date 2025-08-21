17e7f2cc3cd7f5765b5e9be2c5d31710129dd87f
Brian Dellabetta <bdellabe@redhat.com>
2025-08-21 20:48:02 +0000

    use isfinite
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/llmcompressor/modifiers/awq/base.py
+++ src/llmcompressor/modifiers/awq/base.py
@@ -479,10 +479,10 @@ class AWQModifier(Modifier, QuantizationMixin):
                     )
                     del self._smooth_activation_means[mapping.smooth_name]
                     continue
-                if any([fp16_output.isnan().any() for fp16_output in fp16_outputs]):
+                if any([fp16_output.isfinite().any() for fp16_output in fp16_outputs]):
                     logger.warning(
-                        f"Skipping smooth_layer {mapping.smooth_name}, NaN outputs "
-                        "found during forward pass of the parent module "
+                        f"Skipping smooth_layer {mapping.smooth_name}, NaN or inf "
+                        "outputs found during forward pass of the parent module "
                         f"{mapping.parent_name}. The model is either generating NaN "
                         "output with provided calibration data set, or the mappings "
                         "are incorrectly set and modifying the model in undesired "
