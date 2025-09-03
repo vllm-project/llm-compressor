aaad6424ee5e57d7516d5cc851cdf0974169c1d7
Dan Huang <dan.huang@neuralmagic.com>
2025-09-03 17:10:18 -0400

    guard main
    Signed-off-by: Dan Huang <dan.huang@neuralmagic.com>

--- tests/e2e/vLLM/run_vllm.py
+++ tests/e2e/vLLM/run_vllm.py
@@ -5,22 +5,26 @@ import torch
 from vllm import LLM, SamplingParams
 
 
-llm_kwargs = json.loads(sys.argv[1])
-prompts = json.loads(sys.argv[2])
+def main():
+    sampling_params = SamplingParams(temperature=0.80, top_p=0.95)
 
-sampling_params = SamplingParams(temperature=0.80, top_p=0.95)
+    llm = LLM(**llm_kwargs)
+    outputs = llm.generate(prompts, sampling_params)
 
-llm = LLM(**llm_kwargs)
-outputs = llm.generate(prompts, sampling_params)
+    json_outputs = {}
+    for output in outputs:
+        assert output
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        json_outputs[prompt] = generated_text
 
-json_outputs = {}
-for output in outputs:
-    assert output
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    json_outputs[prompt] = generated_text
+    del llm
+    gc.collect()
 
-del llm
-gc.collect()
+    print("VLLMOUTPUT"+str(json_outputs)+"VLLMOUTPUT")
 
-print("VLLMOUTPUT"+str(json_outputs)+"VLLMOUTPUT")
+if __name__ == "__main__":
+    llm_kwargs = json.loads(sys.argv[1])
+    prompts = json.loads(sys.argv[2])
+
+    main()
