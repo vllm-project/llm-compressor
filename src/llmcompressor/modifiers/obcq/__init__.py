8b59b0e964dc9a31ce692019d35ed8455f4d6112
Abhijit <abroy@redhat.com>
2025-09-18 18:09:06 +0530

    Move OBCQ to SparseGPT
    (cherry picked from commit 0eed4a43faeec83619edf2613d5c1cdbff166e10)

--- src/llmcompressor/modifiers/obcq/__init__.py
+++ src/llmcompressor/modifiers/obcq/__init__.py
@@ -1,3 +1,12 @@
-# ruff: noqa
+# Legacy shim for backwards-compat imports 
+import warnings
 
-from .base import *
+warnings.warn(
+    "llmcompressor.modifiers.obcq is deprecated; "
+    "use llmcompressor.modifiers.pruning.sparsegpt instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+# Re-export public API from the new location
+from llmcompressor.modifiers.pruning.sparsegpt import *
