3d40e77a7728d510924e40f4591cb697dcaf7645
Brian Dellabetta <bdellabe@redhat.com>
2025-09-24 15:24:55 +0000

    quality/style
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/llmcompressor/modifiers/obcq/sgpt_base.py
+++ src/llmcompressor/modifiers/obcq/sgpt_base.py
@@ -1,16 +1,19 @@
 from compressed_tensors.utils import deprecated
 
-from llmcompressor.modifiers.pruning.sparsegpt import SparseGPTModifier as PruningSparseGPTModifier
+from llmcompressor.modifiers.pruning.sparsegpt import (
+    SparseGPTModifier as PruningSparseGPTModifier,
+)
 
 __all__ = ["SparseGPTModifier"]
 
 # Legacy shim for backwards-compat imports
 
+
 class SparseGPTModifier(PruningSparseGPTModifier):
     @deprecated(
         message=(
-            "SparseGPTModifier has been moved, please initialize it from "
-            "`llmcompressor.modifiers.pruning.sparsegpt.SparseGPTModifier` in the future"
+            "SparseGPTModifier has been moved. In future, please initialize it "
+            " from `llmcompressor.modifiers.pruning.sparsegpt.SparseGPTModifier`"
         )
     )
     def __new__(cls, *args, **kwargs):
