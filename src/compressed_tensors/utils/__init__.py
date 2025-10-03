07f3b78772d4de6651090e60de4183cff87537d3
Brian Dellabetta <brian-dellabetta@users.noreply.github.com>
2025-10-03 10:13:37 -0400

    Remove deprecated safe_permute (#471)
    * remove deprecated safe_permute
    
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>
    
    * remove test
    
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>
    
    ---------
    
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/compressed_tensors/utils/__init__.py
+++ src/compressed_tensors/utils/__init__.py
@@ -18,7 +18,6 @@ from .internal import *
 from .match import *
 from .offload import *
 from .permutations_24 import *
-from .permute import *
 from .safetensors_load import *
 from .semi_structured_conversions import *
 from .type import *
