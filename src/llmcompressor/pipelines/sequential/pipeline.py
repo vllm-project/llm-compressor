ee7c416d24946635c25bc7b081ef98c7705d92bc
ronantakizawa <ronantakizawa@gmail.com>
2025-10-14 13:58:40 -0600

    Apply code formatting to pipeline.py
    Run make style to format code according to project standards
    
    Signed-off-by: ronantakizawa <ronantakizawa@gmail.com>

--- src/llmcompressor/pipelines/sequential/pipeline.py
+++ src/llmcompressor/pipelines/sequential/pipeline.py
@@ -97,14 +97,9 @@ class SequentialPipeline(CalibrationPipeline):
                 if isinstance(obj, torch.Tensor) and obj.is_meta:
                     return torch.zeros_like(obj, device=model_device)
                 elif isinstance(obj, dict):
-                    return {
-                        k: _materialize_meta_tensors(v)
-                        for k, v in obj.items()
-                    }
+                    return {k: _materialize_meta_tensors(v) for k, v in obj.items()}
                 elif isinstance(obj, (list, tuple)):
-                    return type(obj)(
-                        [_materialize_meta_tensors(x) for x in obj]
-                    )
+                    return type(obj)([_materialize_meta_tensors(x) for x in obj])
                 return obj
 
             for subgraph_index, subgraph in enumerate(subgraphs):
