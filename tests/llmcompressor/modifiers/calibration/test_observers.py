c01bf73c4029b418b2942254d486ef05501f3865
Kyle Sayers <kylesayrs@gmail.com>
2025-10-14 15:42:25 -0400

    [Observers] Refactor for better FP4 support, static and memoryless observers (#1903)
    ## Purpose ##
    * FP4
    * Fix bug discovered
    [here](https://github.com/vllm-project/llm-compressor/pull/1830#discussion_r2358645326)
    where dynamic="local" nvfp4 calculations would increment the observer
    twice as fast as normal
      * Enable MSE observer to be used with FP4
        ```psuedocode
        mse_quant_error := mean((x - fake_quant(x))**2)
    global_scale <- min[min_vals, max_vals,
    global_scale](mse_quant_error(x))
    scale, zp <- min[min_vals, max_vals](mse_quant_error(x, global_scale))
        ```
    * Simplification
    * Make supporting attention calibration easier by separating out
    weight/activation/attention reshaping
    * Improve readability of observer codes by removing many levels of
    function indirection
    * Drop support for calibration with non-divisible group sizes. This is
    not really a loss, since [forward
    passes](https://github.com/neuralmagic/compressed-tensors/blob/main/src/compressed_tensors/quantization/lifecycle/forward.py#L279)
    also make this assumption
    * New observers
    * `memoryless_minmax` computes min and max values on the fly in a
    dynamic-quantization style. This observer is useful for PTQ weight
    quantization
    * `static_minmax` computes absolute min and max values across all
    observations. This observer is useful for PTQ activation quantization
    * `memoryless_mse` computes best qparams w.r.t. MSE loss for each
    observation. This observer is useful for PTQ weight quantization
    * Memory improvements
    * All observers no longer store copies of scales and zero points,
    reducing the amount of required memory
    * Newly introduced "memoryless" observers do not store any quantization
    parameters, which greatly reduces the memory requirements for PTQ weight
    quantization of very large models
    
    | Diagrams |
    | - |
    | Before |
    | <img width="886" height="595" alt="before"
    src="https://github.com/user-attachments/assets/660d94c2-3ac8-4e05-9e9b-53d21145abac"
    /> |
    | After | 
    <img width="1527" height="595" alt="after"
    src="https://github.com/user-attachments/assets/51a0107e-3fbd-413c-a7a6-03ddc3612169"
    /> |
    
    ## Changes ##
    * Standardize reshaping using `flatten_for_calibration`
    * This function reshapes all observed values to `(num_observations,
    *qparams_shape, group_size)`
    * This function the complexity associated with passing "reduce dims" and
    trying to handle weights, activations, and attention states all in the
    same function
    * In the future, this function could be applied to the quantization
    forward pass, although there's probably no need to outside of
    standardization
    * Implement `get_global_scale` on `Observer` base
    * This function decouples minmax calculations from regular qparam
    calculations (avoiding the double increment bug)
    * This function enables the MSE observer to be used with FP4 global
    scales
    
    ## Testing ##
    * Added additional minmax tests which check exact values of scales. This
    test passes both on main and this branch, demonstrating that minmax
    observer behavior remains unchanged
    * Added additional MSE tests which check exact values of mse losses.
    This test passes both on main and this branch, demonstrating that MSE
    observer behavior remains unchanged
    * Added FP4 MSE test
    
    ## Evaluation ##
    ```
    nvfp4-static-minmax
    | Tasks  |Version|Filter|n-shot| Metric |   |Value |   |Stderr|
    |--------|------:|------|-----:|--------|---|-----:|---|------|
    |mmmu_val|      0|none  |     0|mmmu_acc|↑  |0.6167|±  |   N/A|
    ```
    
    ```
    nvfp4-minmax
    | Tasks  |Version|Filter|n-shot| Metric |   |Value |   |Stderr|
    |--------|------:|------|-----:|--------|---|-----:|---|------|
    |mmmu_val|      0|none  |     0|mmmu_acc|↑  |0.6011|±  |   N/A|
    ```
    
    ---------
    
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>
    Signed-off-by: Dan Huang <dan.huang@neuralmagic.com>
    Co-authored-by: dhuangnm <74931910+dhuangnm@users.noreply.github.com>

--- tests/llmcompressor/modifiers/calibration/test_observers.py
+++ tests/llmcompressor/modifiers/calibration/test_observers.py
@@ -7,23 +7,24 @@ from compressed_tensors.quantization import (
 )
 
 from llmcompressor.modifiers.quantization.calibration import initialize_observer
+from llmcompressor.observers import Observer
 
 
 @pytest.mark.parametrize(
     "shape,group_size,actorder",
     [
         ((1, 1), None, False),
-        ((1, 1), 128, False),
-        ((1, 1), 128, True),
+        ((1, 1), 1, False),
+        ((1, 1), 1, True),
         ((64, 64), None, False),
-        ((64, 64), 128, False),
-        ((64, 64), 128, True),
-        ((1792, 4096), None, False),
-        ((1792, 4096), 128, False),
-        ((1792, 4096), 128, True),
-        ((3420, 64), None, False),
-        ((3420, 64), 128, False),
-        ((3420, 64), 128, True),
+        ((64, 64), 32, False),
+        ((64, 64), 32, True),
+        ((896, 4096), None, False),
+        ((896, 4096), 7, False),
+        ((896, 4096), 7, True),
+        ((512, 64), None, False),
+        ((512, 64), 128, False),
+        ((512, 64), 128, True),
     ],
 )
 def test_observers_update(shape, group_size, actorder):
@@ -49,8 +50,7 @@ def test_observers_update(shape, group_size, actorder):
         ("output", output),
     ):
         observer = getattr(module, f"{location}_observer")
-        g_idx = getattr(module, "g_idx", None)
-        updated_scale, updated_zero_point = observer(value, g_idx=g_idx)
+        updated_scale, updated_zero_point = observer(value)
 
         assert_alike(updated_scale, getattr(module, f"{location}_scale"))
         assert_alike(updated_zero_point, getattr(module, f"{location}_zero_point"))
@@ -59,3 +59,67 @@ def test_observers_update(shape, group_size, actorder):
 def assert_alike(a, b):
     assert a.dtype == b.dtype
     assert a.shape == b.shape
+
+
+@pytest.mark.parametrize("is_global", [False, True])
+@pytest.mark.parametrize(
+    "name,kwargs,observed,exp_min_vals,exp_max_vals",
+    (
+        (
+            "memoryless_minmax",
+            {},
+            torch.tensor([[0.0, 0.0], [-3.0, 1.0], [-1.0, 3.0]]),
+            torch.tensor([[0.0], [-3.0], [-1.0]]),
+            torch.tensor([[0.0], [1.0], [3.0]]),
+        ),
+        (
+            "static_minmax",
+            {},
+            torch.tensor([[0.0, 0.0], [-3.0, 1.0], [-1.0, 3.0]]),
+            torch.tensor([[0.0], [-3.0], [-3.0]]),
+            torch.tensor([[0.0], [1.0], [3.0]]),
+        ),
+        (
+            "minmax",  # moving average
+            {"averaging_constant": 0.1},
+            torch.tensor([[0.0, 0.0], [-3.0, 1.0], [-1.0, 3.0]]),
+            torch.tensor([[0.0], [-0.3], [-0.37]]),
+            torch.tensor([[0.0], [0.1], [0.39]]),
+        ),
+        (
+            "memoryless_mse",
+            {},
+            torch.tensor([[0.0, 0.0], [-3.0, 1.0], [-1.0, 3.0]]),
+            torch.tensor([[0.0], [-3.0], [-1.0]]),
+            torch.tensor([[0.0], [1.0], [3.0]]),
+        ),
+        (
+            "mse",  # moving average
+            {"averaging_constant": 0.1},
+            torch.tensor([[0.0, 0.0], [-3.0, 1.0], [-1.0, 3.0]]),
+            torch.tensor([[0.0], [-0.3], [-0.37]]),
+            torch.tensor([[0.0], [0.1], [0.39]]),
+        ),
+    ),
+)
+def test_observer_min_max_vals(
+    name, kwargs, observed, exp_min_vals, exp_max_vals, is_global
+):
+    observer = Observer.load_from_registry(
+        name, base_name="input", args=QuantizationArgs(strategy="tensor"), **kwargs
+    )
+
+    min_vals, max_vals = [], []
+    for _observed in observed:
+        if not is_global:
+            _, _, _min_vals, _max_vals = observer._forward_with_minmax(_observed)
+        else:
+            _, _min_vals, _max_vals = observer._get_global_scale_with_minmax(_observed)
+
+        min_vals.append(_min_vals)
+        max_vals.append(_max_vals)
+
+    min_vals = torch.stack(min_vals)
+    max_vals = torch.stack(max_vals)
+    assert torch.allclose(min_vals, exp_min_vals)
+    assert torch.allclose(max_vals, exp_max_vals)
