f7873a851c59353847d72e3fc8d9a5f5bf1f9f72
HDCharles <39544797+HDCharles@users.noreply.github.com>
2025-10-28 15:11:10 -0400

    weird random-matrix behavior
    Signed-off-by: HDCharles <39544797+HDCharles@users.noreply.github.com>

--- tests/llmcompressor/modifiers/transform/test_correctness.py
+++ tests/llmcompressor/modifiers/transform/test_correctness.py
@@ -21,13 +21,13 @@ from tests.testing_utils import requires_gpu
     "modifier,model_dtype,precision,transform_block_size,exp_mse",
     [
         (QuIPModifier, torch.bfloat16, torch.bfloat16, None, 5e-3),  # 0.0019
-        (QuIPModifier, torch.bfloat16, torch.float32, 16, 5e-3),  # 0.0022
-        (QuIPModifier, torch.float32, torch.float32, 32, 5e-10),  # 1.0e-10
-        (QuIPModifier, torch.float32, torch.float64, 64, 5e-11),  # 2.7e-11
-        (SpinQuantModifier, torch.bfloat16, torch.bfloat16, None, 5e-3),  # 0.0030
-        (SpinQuantModifier, torch.bfloat16, torch.float32, 16, 5e-3),  # 0.0029
-        (SpinQuantModifier, torch.float32, torch.float32, 32, 5e-4),  # 4e-4
-        (SpinQuantModifier, torch.float32, torch.float64, 64, 5e-4),  # 4e-4
+        # (QuIPModifier, torch.bfloat16, torch.float32, 16, 5e-3),  # 0.0022
+        # (QuIPModifier, torch.float32, torch.float32, 32, 5e-10),  # 1.0e-10
+        # (QuIPModifier, torch.float32, torch.float64, 64, 5e-11),  # 2.7e-11
+        # (SpinQuantModifier, torch.bfloat16, torch.bfloat16, None, 5e-3),  # 0.0030
+        # (SpinQuantModifier, torch.bfloat16, torch.float32, 16, 5e-3),  # 0.0029
+        # (SpinQuantModifier, torch.float32, torch.float32, 32, 5e-4),  # 4e-4
+        # (SpinQuantModifier, torch.float32, torch.float64, 64, 5e-4),  # 4e-4
     ],
 )
 def test_apply_correctness(
@@ -37,12 +37,12 @@ def test_apply_correctness(
         "meta-llama/Llama-3.2-1B-Instruct", device_map="cuda", torch_dtype=model_dtype
     )
     untie_word_embeddings(model)
-
     state = State(model=model)
     modifier = modifier(
-        transform_type="random-hadamard",
+        transform_type="random-matrix",
         precision=precision,
         transform_block_size=transform_block_size,
+        ignore = []
     )
 
     input = {k: v.to("cuda") for k, v in model.dummy_inputs.items()}
