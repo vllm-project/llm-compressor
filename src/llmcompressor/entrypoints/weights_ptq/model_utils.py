fb739cfa16bc85956a7f050863dd82d87c14b66d
Kyle Sayers <kylesayrs@gmail.com>
2025-10-30 03:18:17 +0000

    walk path with ignore
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/entrypoints/weights_ptq/model_utils.py
+++ src/llmcompressor/entrypoints/weights_ptq/model_utils.py
@@ -22,19 +22,20 @@ def get_checkpoint_files(model_stub: str | os.PathLike) -> list[str]:
     # In the future, this function can accept and pass download kwargs to cached_file
 
     if os.path.exists(model_stub):
-        file_paths = walk_file_paths(model_stub)
+        file_paths = walk_file_paths(model_stub, ignore=".cache")
     else:
         file_paths = list_repo_files(model_stub)
 
     return [(file_path, cached_file(model_stub, file_path)) for file_path in file_paths]
 
 
-def walk_file_paths(root_dir: str) -> list[str]:
+def walk_file_paths(root_dir: str, ignore: str | None = None) -> list[str]:
     all_files = []
-    for dirpath, _dirnames, filenames in os.walk(root_dir):
+    for dirpath, _, filenames in os.walk(root_dir):
         for filename in filenames:
-            full_path = os.path.join(dirpath, filename)
-            all_files.append(full_path)
+            rel_path = os.path.relpath(os.path.join(dirpath, filename), root_dir)
+            if not (ignore and rel_path.startswith(ignore)):
+                all_files.append(rel_path)
     return all_files
 
 
