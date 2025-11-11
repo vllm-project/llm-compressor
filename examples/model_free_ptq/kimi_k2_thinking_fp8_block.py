eb13e63b68409d5de71561f0f4196db7e5f9f867
Dipika Sikka <dipikasikka1@gmail.com>
2025-11-11 13:53:51 -0500

    update

--- examples/model_free_ptq/kimi_k2_thinking_fp8_block.py
+++ examples/model_free_ptq/kimi_k2_thinking_fp8_block.py
@@ -3,7 +3,9 @@ from llmcompressor import model_free_ptq
 MODEL_ID = "unsloth/Kimi-K2-Thinking-BF16"
 SAVE_DIR = "Kimi-K2-Thinking-FP8-Block"
 
-# Apply quantization.
+# Apply FP8-Block to the model
+# Once quantized, the model is saved
+# using compressed-tensors to the SAVE_DIR.
 model_free_ptq(
     model_stub=MODEL_ID,
     save_directory=SAVE_DIR,
