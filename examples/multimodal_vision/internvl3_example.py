c32d0a52e56aa0d356c6ab02844050529c6200e9
fangxuwei <fangxuwei@ezviz.com>
2025-11-18 12:31:35 +0800

    make style and make quality

--- examples/multimodal_vision/internvl3_example.py
+++ examples/multimodal_vision/internvl3_example.py
@@ -1,12 +1,15 @@
 import torch
 from datasets import load_dataset
-from transformers import AutoProcessor, AutoModelForImageTextToText
+from transformers import AutoModelForImageTextToText, AutoProcessor
+
 from llmcompressor import oneshot
 from llmcompressor.modifiers.quantization import GPTQModifier
 
 # Load model.
 model_id = "OpenGVLab/InternVL3-8B-hf"
-model = AutoModelForImageTextToText.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+model = AutoModelForImageTextToText.from_pretrained(
+    model_id, torch_dtype=torch.bfloat16
+)
 processor = AutoProcessor.from_pretrained(model_id)
 
 # Load datasets
@@ -17,22 +20,28 @@ MAX_SEQUENCE_LENGTH = 512
 ds = load_dataset(DATASET_ID, split=f"{DATASET_SPLIT}[:{NUM_CALIBRATION_SAMPLES}]")
 ds = ds.shuffle(seed=42)
 
+
 def preprocess_and_tokenize(example):
     messages = [
         {
             "role": "user",
             "content": [
-                {
-                    "type": "text", 
-                    "text":  example["messages"]
-                },
+                {"type": "text", "text": example["messages"]},
             ],
         }
     ]
-    inputs = processor.apply_chat_template(messages, add_generation_prompt=True, tokenize=True, return_dict=True, return_tensors="pt")
+    inputs = processor.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    )
     return inputs
 
+
 ds = ds.map(preprocess_and_tokenize)
+
 
 def data_collator(batch):
     assert len(batch) == 1
@@ -42,12 +51,13 @@ def data_collator(batch):
 
     return item
 
+
 # Recipe
 recipe = GPTQModifier(
-        targets="Linear",
-        scheme="FP8",
-        ignore=["re:.*lm_head",  "re:.*vision_tower.*",  "re:.*multi_modal_projector.*"]
-    )
+    targets="Linear",
+    scheme="FP8",
+    ignore=["re:.*lm_head", "re:.*vision_tower.*", "re:.*multi_modal_projector.*"],
+)
 
 # Perform oneshot
 oneshot(
@@ -58,7 +68,7 @@ oneshot(
     max_seq_length=MAX_SEQUENCE_LENGTH,
     num_calibration_samples=NUM_CALIBRATION_SAMPLES,
     trust_remote_code_model=True,
-    data_collator=data_collator
+    data_collator=data_collator,
 )
 
 # Save to disk compressed.
