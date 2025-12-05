a9bd7ba659e1169ac7f66576e19524616148321e
Dan Huang <dahuang@redhat.com>
2025-12-05 13:30:20 -0500

    update auto-round due to its limit for accelerate in 0.9.1
    Signed-off-by: Dan Huang <dahuang@redhat.com>

--- setup.py
+++ setup.py
@@ -129,9 +129,9 @@ setup(
         ),
         ("datasets>=4.0.0,<=4.4.1" if BUILD_TYPE == "release" else "datasets>=4.0.0"),
         (
-            "auto-round>=0.9.1,<=0.9.2"
+            "auto-round>=0.9.2,<=0.9.2"
             if BUILD_TYPE == "release"
-            else "auto-round>=0.9.1"
+            else "auto-round>=0.9.2"
         ),
         (
             "accelerate>=1.6.0,<=1.12.0"
