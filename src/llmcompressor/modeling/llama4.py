49a0cc88471810a678bc88c9a9c8716ff35b42f6
Dipika Sikka <dipikasikka1@gmail.com>
2025-12-09 20:15:56 -0500

    update

--- src/llmcompressor/modeling/llama4.py
+++ src/llmcompressor/modeling/llama4.py
@@ -45,27 +45,38 @@ class SequentialLlama4TextMoe(MoECalibrationModule):
         self.router = original.router
         self.shared_expert = original.shared_expert
         self.calibrate_all_experts = calibrate_all_experts
-        print(self.calibrate_all_experts, "CALIBRATE ALL EXPERTS")
 
     def forward(self, hidden_states: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         hidden_states = hidden_states.reshape(-1, self.hidden_dim)
-        router_logits = self.router(hidden_states)
-        # support transformers 4.53 and greater
-        if isinstance(router_logits, tuple):
-            router_logits = router_logits[-1]
-
-        router_top_value, router_indices = torch.topk(router_logits, self.top_k, dim=1)
-
-        router_scores = (
-            torch.full_like(router_logits, float("-inf"))
-            .scatter_(1, router_indices, router_top_value)
-            .transpose(0, 1)
-        )
-        router_scores = torch.sigmoid(router_scores.float()).to(hidden_states.dtype)
-
+        router_scores, router_logits = self.router(hidden_states)
         out = self.shared_expert(hidden_states)
+
+        _, router_indices = torch.topk(router_logits, self.top_k, dim=1)
+        expert_mask = torch.nn.functional.one_hot(
+            router_indices, num_classes=self.num_experts
+        ).permute(2, 1, 0)  #  (num_experts, top_k, batch_size * sequence_length)
+
         for i in range(self.num_experts):
-            out += self.experts[i](hidden_states) * router_scores[i].reshape(-1, 1)
+            # fetch relevant token indices for this expert
+            token_idx = torch.where(expert_mask[i].squeeze(0))
+
+            # Original Llama4 definition - apply score to hidden states before applying to expert
+            # this results in NaNs during calibration
+            # routed_in = hidden_states * router_scores[:, i].reshape(-1, 1)
+
+            if self.calibrate_all_experts:
+                # all tokens for this expert
+                expert_out = self.experts[i](hidden_states)[token_idx]
+            else:
+                # only relevant tokens for this expert
+                expert_out = self.experts[i](hidden_states[token_idx])
+
+            if len(token_idx) > 0:
+                # Deviation from original Llama4 definition to avoid NaNs during calibration
+                weighted_output = expert_out * router_scores[:, i][token_idx].reshape(
+                    -1, 1
+                )
+                out[token_idx] += weighted_output
 
         return out, router_logits
 
