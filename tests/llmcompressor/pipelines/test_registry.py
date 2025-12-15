6b29cd6df33f4fcd8042385972b687d09497e15b
Kyle Sayers <kylesayrs@gmail.com>
2025-12-15 19:31:31 +0000

    update tests
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/llmcompressor/pipelines/test_registry.py
+++ tests/llmcompressor/pipelines/test_registry.py
@@ -1,23 +1,28 @@
 import pytest
 
-from llmcompressor.modifiers.quantization import QuantizationModifier
-from llmcompressor.pipelines import (
-    CalibrationPipeline,
-    SequentialPipeline,
-)
+from llmcompressor.modifiers.quantization import QuantizationModifier, GPTQModifier
+from llmcompressor.modifiers.smoothquant import SmoothQuantModifier
+from llmcompressor.modifiers.awq import AWQModifier
+from llmcompressor.modifiers.pruning import SparseGPTQModifier, WandaPruningModifier
+from llmcompressor.modifiers.transform import QuIPModifier, SpinQuantModifier
+from llmcompressor.pipelines import CalibrationPipeline, SequentialPipeline, DataFreePipeline
 
 
-@pytest.mark.parametrize(
-    "modifiers",
-    [
-        (
-            [
-                QuantizationModifier(),
-            ],
-            SequentialPipeline,
-        )
-    ],
-)
+@pytest.mark.parametrize("modifiers", [
+    ([QuantizationModifier(scheme="FP8")], SequentialPipeline)
+    ([QuantizationModifier(scheme="W4A16")], DataFreePipeline)
+    ([GPTQModifier(scheme="FP8")], SequentialPipeline)
+    ([GPTQModifier(scheme="W4A16")], DataFreePipeline)
+    ([SmoothQuantModifier(), GPTQModifier(scheme="W4A16")], SequentialPipeline),
+    ([AWQModifier(scheme="W4A16")], SequentialPipeline)
+    ([AWQModifier(scheme="FP8")], SequentialPipeline)
+    ([SparseGPTQModifier()], SequentialPipeline)
+    ([WandaPruningModifier()], SequentialPipeline)
+    ([QuIPModifier()], DataFreePipeline)
+    ([SpinQuantModifier()], DataFreePipeline)
+    ([QuIPModifier(), QuantizationModifier(scheme="FP8")], SequentialPipeline)
+    ([QuIPModifier(), QuantizationModifier(scheme="W4A16")], DataFreePipeline)
+])
 def test_infer_pipeline(modifiers, exp_pipeline):
     pipeline = CalibrationPipeline.from_modifiers(modifiers)
     assert isinstance(pipeline, exp_pipeline)
