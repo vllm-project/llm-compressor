314836081c6c6fb6a096eaffc8ec1ab4c146ceaf
HDCharles <charlesdavidhernandez@gmail.com>
2025-12-17 21:05:47 +0000

    fp8 awq examples
    Summary
    
    Signed-off-by: HDCharles <charlesdavidhernandez@gmail.com>

---
+++ examples/awq/fp8_block_llama_example.py
@@ -0,0 +1,43 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from llmcompressor import oneshot
+from llmcompressor.modifiers.quantization import QuantizationModifier
+from llmcompressor.utils import dispatch_for_generation
+
+# Select model and load it.
+MODEL_ID = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+model = AutoModelForCausalLM.from_pretrained(MODEL_ID, torch_dtype="auto")
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, trust_remote_code=True)
+
+# Configure the quantization algorithm to run.
+# In this case, we:
+#   * quantize the weights to fp8 with per channel via ptq
+#   * quantize the activations to fp8 with blockwise quantization
+recipe = [
+    QuantizationModifier(
+        targets="Linear", scheme="FP8_BLOCK", ignore=["lm_head"]
+    ),
+]
+
+# Apply algorithms.
+oneshot(
+    model=model,
+    recipe=recipe,
+)
+
+# Confirm generations of the quantized model look sane.
+print("\n\n")
+print("========== SAMPLE GENERATION ==============")
+dispatch_for_generation(model)
+input_ids = tokenizer("Hello my name is", return_tensors="pt").input_ids.to(
+    model.device
+)
+output = model.generate(input_ids, max_new_tokens=100)
+print(tokenizer.decode(output[0]))
+print("==========================================\n\n")
+
+# Save to disk compressed.
+SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-fp8-block"
+model.save_pretrained(SAVE_DIR, save_compressed=True)
+tokenizer.save_pretrained(SAVE_DIR)
