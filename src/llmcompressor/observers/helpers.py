dcb2e8d909e77a7c6757d45a48cf2a7be9084939
Brian Dellabetta <bdellabe@redhat.com>
2026-01-26 19:28:39 +0000

    move to helper, unit test
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/llmcompressor/observers/helpers.py
+++ src/llmcompressor/observers/helpers.py
@@ -11,6 +11,7 @@ import math
 from typing import Optional
 
 import torch
+import torch.nn.functional as F
 from compressed_tensors.quantization import QuantizationArgs, QuantizationStrategy
 from compressed_tensors.quantization.utils import strategy_cdiv
 
@@ -77,41 +78,11 @@ def _flatten_weight(
         # (1, num_block_rows, num_block_cols, block_width * block_height)
         block_height, block_width = args.block_structure
         rows, cols = value.shape
-        # If shape is incompatible, pad to compatible shape with the median value of the block
+        # If shape is incompatible, pad to compatible shape with the median value
+        # of the block so as not to distort resultant qparams
         if rows % block_height != 0 or cols % block_width != 0:
-            block_rows = math.ceil(rows / block_height)
-            block_cols = math.ceil(cols / block_width)
-            padding_rows = block_height - rows
-            padding_cols = block_width - cols
-
-            new_value = torch.zeros((padding_rows, padding_cols), dtype=value.dtype)
-            new_value[0:rows, 0:cols] = value
-            value = new_value
+            value = _pad_to_block_size_with_mean(value, args.block_structure)
             rows, cols = value.shape
-
-            # pad last rows along each column
-            for block_col_idx in range(block_cols - 1):
-                value[
-                    (rows - padding_rows) :,
-                    block_col_idx * block_width : (1 + block_col_idx) * block_width,
-                ] = value[
-                    (rows - block_height) : (rows - padding_rows),
-                    block_col_idx * block_width : (1 + block_col_idx) * block_width,
-                ].median()
-            # pad last columns along each row
-            for block_row_idx in range(block_rows - 1):
-                value[
-                    block_row_idx * block_height : (1 + block_row_idx) * block_height,
-                    (cols - padding_cols) :,
-                ] = value[
-                    block_row_idx * block_height : (1 + block_row_idx) * block_height,
-                    (cols - block_width) : (cols - padding_cols),
-                ].median()
-            # pad block in last row and last column
-            value[rows - padding_rows :, :] = value[:, cols - padding_cols :] = value[
-                rows - block_height : rows - padding_rows,
-                cols - block_width : cols - padding_cols,
-            ].median()
 
         block_rows = strategy_cdiv(rows, block_height, args.strategy, strict=True)
         block_cols = strategy_cdiv(cols, block_width, args.strategy, strict=True)
@@ -181,3 +152,63 @@ def _flatten_attention(value: torch.Tensor, args: QuantizationArgs):
         return value.transpose(1, 2).flatten(0, 1).unsqueeze(-2).unsqueeze(-2)
 
     raise ValueError(f"Unknown strategy {args.strategy}")
+
+
+def _pad_to_block_size_with_mean(
+    value: torch.Tensor, block_size: tuple[int, int]
+) -> torch.Tensor:
+    """
+    Given an input 2d tensor and a desired block size,
+    return a padded tensor that is evenly divisible by
+    the block size and with all padded values being equal
+    to the mean value of the filled block, so as to avoid
+    altering qparams calculations like min, max, mean.
+
+    :param value: tensor to be padded
+    :param block_size: desired block size, height x width
+    :return: padded tensor
+    """
+    block_height, block_width = block_size
+    rows, cols = value.shape
+    block_rows = math.ceil(rows / block_height)
+    block_cols = math.ceil(cols / block_width)
+    padding_rows = block_rows * block_height - rows
+    padding_cols = block_cols * block_width - cols
+
+    if padding_rows == 0 and padding_cols == 0:
+        return value
+
+    # pad to new tensor with zeros at the end (right-pad)
+    value = F.pad(value, (0, padding_cols, 0, padding_rows), "constant", value=0.0)
+    rows, cols = value.shape
+
+    # pad last rows along each column, except for bottom right
+    for block_col_idx in range(block_cols - 1):
+        value[
+            (rows - padding_rows) :,
+            block_col_idx * block_width : (1 + block_col_idx) * block_width,
+        ] = value[
+            (rows - block_height) : (rows - padding_rows),
+            block_col_idx * block_width : (1 + block_col_idx) * block_width,
+        ].mean()
+    # pad last columns along each row, except for bottom right
+    for block_row_idx in range(block_rows - 1):
+        value[
+            block_row_idx * block_height : (1 + block_row_idx) * block_height,
+            (cols - padding_cols) :,
+        ] = value[
+            block_row_idx * block_height : (1 + block_row_idx) * block_height,
+            (cols - block_width) : (cols - padding_cols),
+        ].mean()
+    # pad bottom-right block (last row and last column)
+    last_mean = value[
+        rows - block_height : rows - padding_rows,
+        cols - block_width : cols - padding_cols,
+    ].mean()
+    value[rows - block_height :, (cols - padding_cols) :] = last_mean
+    value[(rows - padding_rows) :, cols - block_width :] = last_mean
+
+    return value
+
+
+
