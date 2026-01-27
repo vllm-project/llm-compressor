84d61085a22f803cfcaa2e31837625db2dea9d98
Itay Etelis <itay.etelis@ibm.com>
2026-01-27 20:53:37 +0200

    Fix build_parameterized_layers to use match_named_modules
    - Fix match_named_parameters not matching class names (e.g., "Linear")
      by switching to match_named_modules which supports class name matching
    - Inline match_modules function as it was only used in one place
    - Remove match_modules function from helpers.py
    
    Signed-off-by: Itay Etelis <itay.etelis@ibm.com>

--- src/llmcompressor/pipelines/sequential/helpers.py
+++ src/llmcompressor/pipelines/sequential/helpers.py
@@ -101,7 +101,9 @@ def trace_subgraphs(
     :return: a list of Subgraphs in order of execution
     """
     # find modules
-    targets = match_modules(model, sequential_targets)
+    targets = set(
+        module for _, module in match_named_modules(model, sequential_targets)
+    )
     ancestors = get_sequential_ancestors(model, targets)
     offloaded = set()  # TODO: cleanup logic
 
@@ -425,17 +427,6 @@ def graph_is_well_formed(graph: Graph) -> bool:
             return False
 
     return True
-
-
-def match_modules(model: Module, target_names: list[str]) -> set[Module]:
-    """
-    Find modules whose names match the patterns given by `target_names`
-
-    :param model: model containing submodules to find
-    :param target_names: target patterns to find
-    :return: all submodules matching `target_names`
-    """
-    return set(module for _, module in match_named_modules(model, target_names))
 
 
 def get_sequential_targets(
