920a041392e0c7cb5f1e7a3a3e6149b793a49acc
Itay Etelis <itay.etelis@ibm.com>
2026-01-30 17:26:22 +0200

    Remove linear_attn.norm mapping that causes shape mismatch
    Signed-off-by: Itay Etelis <itay.etelis@ibm.com>

--- src/llmcompressor/modifiers/awq/mappings.py
+++ src/llmcompressor/modifiers/awq/mappings.py
@@ -71,7 +71,6 @@ _qwen3_next_moe_mappings = [
         "re:.*layers\\.(0|1|2|4|5|6|8|9|10|12|13|14|16|17|18|20|21|22|24|25|26|28|29|30|32|33|34|36|37|38|40|41|42|44|45|46)\\.input_layernorm$",
         ["re:.*linear_attn.in_proj_qkvz$", "re:.*linear_attn.in_proj_ba$"],
     ),
-    AWQMapping("re:.*linear_attn.norm$", ["re:.*linear_attn.out_proj$"]),
     AWQMapping(
         "re:.*post_attention_layernorm$",
         [
