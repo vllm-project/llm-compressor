dd19579d6c68a2c5767a90f744c3720393d81f4f
Brian Dellabetta <bdellabe@redhat.com>
2026-02-17 18:31:01 +0000

    exclude comment out of failing function
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/llmcompressor/modifiers/quantization/quantization/base.py
+++ src/llmcompressor/modifiers/quantization/quantization/base.py
@@ -83,8 +83,8 @@ class QuantizationModifier(Modifier, QuantizationMixin):
         # on targeted modules, we need to run on all modules.
         # Because this call is idempotent, setting all global_scales to the
         # min value, it is ok to run potentially multiple times for all modules
-        # for module in state.model.modules():
-        #     update_fused_layer_weight_global_scales(module)
+        for module in state.model.modules():
+            update_fused_layer_weight_global_scales(module)
 
         for _, module in tqdm.tqdm(named_modules, desc="Calibrating weights"):
             update_weight_zp_scale(module)
