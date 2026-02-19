fa82d04b87c5e6b42b92a8e2a0f3e8d67f90b6fc
HDCharles <charlesdavidhernandez@gmail.com>
2026-02-19 14:46:59 -0500

    remove unneeded and test updates
    Summary
    
    Signed-off-by: HDCharles <charlesdavidhernandez@gmail.com>

--- src/llmcompressor/datasets/utils.py
+++ src/llmcompressor/datasets/utils.py
@@ -254,33 +254,10 @@ def _get_partition_start_end(
     return start, end
 
 
-
 def _make_sampler(args: DatasetArguments, dataset: Dataset) -> Sampler:
     num_samples = args.num_calibration_samples
     shuffle = args.shuffle_calibration_samples
     batch_size = args.batch_size
-
-    if torch.distributed.is_initialized() and torch.distributed.get_world_size() > 1:
-        from torch.utils.data.distributed import DistributedSampler
-
-        distributed_sampler = DistributedSampler(
-            dataset,
-            num_replicas=torch.distributed.get_world_size(),
-            rank=torch.distributed.get_rank(),
-            shuffle=shuffle,
-        )
-        logger.warning("Using DistributedSampler for DDP training.")
-
-        def show_distributed_sampler_info(distributed_sampler):
-            logger.warning(
-                f"DistributedSampler: num_replicas={distributed_sampler.num_replicas}, "
-                f"rank={distributed_sampler.rank}, "
-                f"dataset_len={len(dataset)}, "
-                f"num_samples={distributed_sampler.num_samples}"
-            )
-
-        show_distributed_sampler_info(distributed_sampler)
-        return distributed_sampler
 
     # detect whether we're in a distributed setting
     # but all ranks have the same dataset.
