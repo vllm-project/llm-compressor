fe4e28a54150534a769cc4f728f5f357079a4155
Dipika Sikka <dipikasikka1@gmail.com>
2026-02-22 00:26:38 -0500

    update

--- examples/quantization_w4a4_fp4/qwen3_5_moe.py
+++ examples/quantization_w4a4_fp4/qwen3_5_moe.py
@@ -2,6 +2,7 @@ from transformers import AutoProcessor, Qwen3_5MoeForConditionalGeneration
 from datasets import load_dataset
 from llmcompressor import oneshot
 from llmcompressor.modifiers.quantization import QuantizationModifier
+import torch
 
 MODEL_ID = "/raid/engine/dsikka/models--Qwen--Qwen3.5-397B-A17B/snapshots/7cad2bae11cb49ca79f7d6a0954de2e2756f4e27"
 
@@ -25,48 +26,57 @@ recipe = QuantizationModifier(
     ],
 )
 
-DATASET_ID = "HuggingFaceH4/ultrachat_200k"
-DATASET_SPLIT = "train_sft"
-
-# Select number of samples
+DATASET_ID = "neuralmagic/calibration"
 NUM_CALIBRATION_SAMPLES = 20
-MAX_SEQUENCE_LENGTH = 2048
+MAX_SEQUENCE_LENGTH = 8192
 
-# Load dataset and preprocess.
-ds = load_dataset(DATASET_ID, split=f"{DATASET_SPLIT}[:{NUM_CALIBRATION_SAMPLES}]")
-ds = ds.shuffle(seed=42)
+ds = load_dataset(DATASET_ID, name="LLM", split=f"train[:{NUM_CALIBRATION_SAMPLES}]")
 
 
-def preprocess(example):
-    return {
-        "text": processor.apply_chat_template(
-            example["messages"],
-            tokenize=False,
+def preprocess_function(example):
+    messgages = []
+    for message in example["messages"]:
+        messgages.append(
+            {
+                "role": message["role"],
+                "content": [{"type": "text", "text": message["content"]}],
+            }
         )
-    }
 
-
-ds = ds.map(preprocess)
-
-
-# Tokenize inputs.
-def tokenize(sample):
-    return processor(
-        sample["text"],
+    return processor.apply_chat_template(
+        messgages,
+        return_tensors="pt",
         padding=False,
-        max_length=MAX_SEQUENCE_LENGTH,
         truncation=True,
+        max_length=MAX_SEQUENCE_LENGTH,
+        tokenize=True,
         add_special_tokens=False,
+        return_dict=True,
+        add_generation_prompt=False,
     )
 
 
-ds = ds.map(tokenize, remove_columns=ds.column_names)
+ds = ds.map(preprocess_function, batched=False, remove_columns=ds.column_names)
+
+
+def data_collator(batch):
+    assert len(batch) == 1
+    return {
+        key: (
+            torch.tensor(value)
+            if key != "pixel_values"
+            else torch.tensor(value, dtype=torch.bfloat16).squeeze(0)
+        )
+        for key, value in batch[0].items()
+    }
+
 
 
 # Apply quantization.
 oneshot(model=model, 
     recipe=recipe, 
-    dataset=ds,     
+    dataset=ds,
+    data_collator=data_collator,
     max_seq_length=MAX_SEQUENCE_LENGTH,
     num_calibration_samples=NUM_CALIBRATION_SAMPLES,
     moe_calibrate_all_experts=True)
