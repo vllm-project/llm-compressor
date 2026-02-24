5b520d54a1ea4124b17f23da0f34af6ade73d27f
Brian Dellabetta <bdellabe@redhat.com>
2026-02-24 18:56:48 +0000

    stylefix
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- examples/quantization_w8a8_fp8/qwen3_next_example.py
+++ examples/quantization_w8a8_fp8/qwen3_next_example.py
@@ -30,7 +30,6 @@ recipe = QuantizationModifier(
 # Apply quantization.
 oneshot(model=model, recipe=recipe)
 
-
 # Save to disk in compressed-tensors format.
 SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-FP8-Dynamic"
 model.save_pretrained(SAVE_DIR, save_compressed=True)
