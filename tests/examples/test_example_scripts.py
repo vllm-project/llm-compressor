7bedf050daf2068359e45164d0ffd7d96c144a22
Dipika Sikka <dipikasikka1@gmail.com>
2026-02-25 14:43:01 -0500

    fix

--- tests/examples/test_example_scripts.py
+++ tests/examples/test_example_scripts.py
@@ -15,10 +15,8 @@ from tests.testing_utils import requires_gpu, run_cli_command
 
 def verify_quantization_config(tmp_path: Path, prefix: str, compressed_format: str):
     # verify the expected directory was generated
-    dirs: List[Path] = [p for p in tmp_path.rglob(f"-{prefix}") if p.is_dir()]
-    assert (
-        len(dirs)
-    ) == 1, f"did not find exactly one generated folder: {dirs}"
+    dirs: List[Path] = [p for p in tmp_path.rglob(f"*-{prefix}") if p.is_dir()]
+    assert (len(dirs)) == 1, f"did not find exactly one generated folder: {dirs}"
 
     # verify the format in the generated config
     config_json = json.loads((dirs[0] / "config.json").read_text())
@@ -30,14 +28,20 @@ class TestCase(NamedTuple):
     path: str
     flags: tuple[str] = ()
     preprocess_fn: None | Callable[[str], str] = None
-    # verify_fn(tmp_path, example_dir)
-    verify_fn: Callable[[Path, str], None] | None = verify_quantization_config
+    # verify_fn(tmp_path, prefix, compressed_format)
+    verify_fn: Callable[[Path, str, str], None] | None = verify_quantization_config
     compressed_format: str | None = None
     prefix: str | None = None
 
     def __repr__(self):
         values = [f"'{self.path}'"]
-        for attr_name in ["flags", "preprocess_fn", "verify_fn"]:
+        for attr_name in [
+            "flags",
+            "preprocess_fn",
+            "verify_fn",
+            "prefix",
+            "compressed_format",
+        ]:
             attr = getattr(self, attr_name)
             if attr:
                 if callable(attr):
@@ -47,7 +51,7 @@ class TestCase(NamedTuple):
 
                 values.append(f"{attr_name}={attr_repr}")
 
-        return f"{self.__class__.__name__}({', '.join(values)}, prefix='{self.prefix}', compressed_format='{self.compressed_format}')"
+        return f"{self.__class__.__name__}({', '.join(values)})"
 
 
 @pytest.mark.example
@@ -55,31 +59,86 @@ class TestCase(NamedTuple):
 @pytest.mark.parametrize(
     "test_case",
     [
-        TestCase("awq/llama_example.py", compressed_format=CompressionFormat.pack_quantized, prefix="awq-asym"),
-        TestCase("awq/qwen3_moe_example.py", compressed_format=CompressionFormat.pack_quantized, prefix="awq-asym"),
-        TestCase("big_models_with_sequential_onloading/llama3.3_70b.py", compressed_format=CompressionFormat.pack_quantized, prefix="W8A8"),
-        TestCase("quantization_kv_cache/llama3_fp8_kv_example.py", compressed_format=CompressionFormat.float_quantized, prefix="FP8-KV"),
-        TestCase("quantization_w4a16/llama3_example.py", compressed_format=CompressionFormat.pack_quantized, prefix="W4A16-G128"),
-        TestCase("quantization_w8a8_fp8/fp8_block_example.py", compressed_format=CompressionFormat.float_quantized, prefix="FP8-BLOCK"),
-        TestCase("quantization_w8a8_fp8/llama3_example.py", compressed_format=CompressionFormat.float_quantized, prefix="FP8-Dynamic"),
-        TestCase("quantization_w8a8_int8/llama3_example.py", compressed_format=CompressionFormat.int_quantized, prefix="W8A8-Dynamic-Per-Token"),
-        TestCase("quantization_w8a8_fp8/qwen3_vl_moe_fp8_example.py", compressed_format=CompressionFormat.float_quantized, prefix="FP8-DYNAMIC"),
-        TestCase("disk_offloading/qwen3_example.py", compressed_format=CompressionFormat.nvfp4_pack_quantized, prefix="NVFP4"),
-        TestCase("model_free_ptq/qwen3_fp8_block.py", compressed_format=CompressionFormat.float_quantized, prefix="FP8_BLOCK"),
-        TestCase("multimodal_vision/qwen3_vl_example.py", compressed_format=CompressionFormat.pack_quantized, prefix="W4A16"),
-        TestCase("quantization_w4a16_fp4/mxfp4/qwen3_example.py", compressed_format=CompressionFormat.mxfp4_pack_quantized, prefix="MXFP4A16"),
+        TestCase(
+            "awq/llama_example.py",
+            compressed_format=CompressionFormat.pack_quantized,
+            prefix="awq-asym",
+        ),
+        TestCase(
+            "awq/qwen3_moe_example.py",
+            compressed_format=CompressionFormat.pack_quantized,
+            prefix="awq-asym",
+        ),
+        TestCase(
+            "big_models_with_sequential_onloading/llama3.3_70b.py",
+            compressed_format=CompressionFormat.int_quantized,
+            prefix="W8A8",
+        ),
+        TestCase(
+            "quantization_kv_cache/llama3_fp8_kv_example.py",
+            compressed_format=CompressionFormat.float_quantized,
+            prefix="FP8-KV",
+        ),
+        TestCase(
+            "quantization_w4a16/llama3_example.py",
+            compressed_format=CompressionFormat.pack_quantized,
+            prefix="W4A16-G128",
+        ),
+        TestCase(
+            "quantization_w8a8_fp8/fp8_block_example.py",
+            compressed_format=CompressionFormat.float_quantized,
+            prefix="FP8-BLOCK",
+        ),
+        TestCase(
+            "quantization_w8a8_fp8/llama3_example.py",
+            compressed_format=CompressionFormat.float_quantized,
+            prefix="FP8-Dynamic",
+        ),
+        TestCase(
+            "quantization_w8a8_fp8/qwen3_vl_moe_fp8_example.py",
+            compressed_format=CompressionFormat.float_quantized,
+            prefix="FP8-DYNAMIC",
+        ),
+        TestCase(
+            "disk_offloading/qwen3_example.py",
+            compressed_format=CompressionFormat.nvfp4_pack_quantized,
+            prefix="NVFP4",
+        ),
+        TestCase(
+            "model_free_ptq/qwen3_fp8_block.py",
+            compressed_format=CompressionFormat.float_quantized,
+            prefix="FP8_BLOCK",
+        ),
+        TestCase(
+            "multimodal_vision/qwen3_vl_example.py",
+            compressed_format=CompressionFormat.pack_quantized,
+            prefix="W4A16",
+        ),
+        TestCase(
+            "quantization_w4a16_fp4/mxfp4/qwen3_example.py",
+            compressed_format=CompressionFormat.mxfp4_pack_quantized,
+            prefix="MXFP4A16",
+        ),
         TestCase(
             "sparse_2of4_quantization_fp8/llama3_8b_2of4.py",
             flags=["--fp8"],
+            compressed_format=CompressionFormat.float_quantized,
+            prefix="2of4-W8A8-FP8-Dynamic-Per-Token",
         ),
         TestCase(
-            "quantization_w4a4_fp4/llama4_example.py", compressed_format=CompressionFormat.nvfp4_pack_quantized, prefix="NVFP4"
+            "quantization_w4a4_fp4/llama4_example.py",
+            compressed_format=CompressionFormat.nvfp4_pack_quantized,
+            prefix="NVFP4",
         ),
         TestCase(
-            "quantization_w4a4_fp4/qwen_30b_a3b.py", compressed_format=CompressionFormat.nvfp4_pack_quantized, prefix="NVFP4"
+            "quantization_w4a4_fp4/qwen_30b_a3b.py",
+            compressed_format=CompressionFormat.nvfp4_pack_quantized,
+            prefix="NVFP4",
         ),
         TestCase(
-            "quantization_w4a4_fp4/llama3_gptq_example.py", compressed_format=CompressionFormat.nvfp4_pack_quantized, prefix="NVFP4"
+            "quantization_w4a4_fp4/llama3_gptq_example.py",
+            compressed_format=CompressionFormat.nvfp4_pack_quantized,
+            prefix="NVFP4-GPTQ",
         ),
     ],
     ids=repr,
@@ -112,4 +171,4 @@ def test_example_scripts(test_case: str | TestCase, tmp_path: Path):
     )
 
     if test_case.verify_fn:
-        test_case.verify_fn(tmp_path, example_dir)
+        test_case.verify_fn(tmp_path, test_case.prefix, test_case.compressed_format)
