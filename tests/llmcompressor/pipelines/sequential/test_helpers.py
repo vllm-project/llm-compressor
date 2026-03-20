a0ef1053b931f6890aeb86a7c2042b0ef0405ba4
Ayush <aayush7511@gmail.com>
2026-03-20 00:40:56 -0500

    test: added tests for topological_partition with targets_per_subgraph

--- tests/llmcompressor/pipelines/sequential/test_helpers.py
+++ tests/llmcompressor/pipelines/sequential/test_helpers.py
@@ -1,6 +1,9 @@
 import torch
-
+import torch.fx
 from llmcompressor.pipelines.sequential.helpers import get_sequential_ancestors
+from llmcompressor.pipelines.sequential.helpers import topological_partition
+import pytest
+
 
 
 class DummyModel(torch.nn.Module):
@@ -13,7 +16,26 @@ class DummyModel(torch.nn.Module):
         x = self.seq(x)
         return self.fc(x)
 
-
+class DummyModelMultipleSequentialLayers(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = torch.nn.Linear(10, 10)
+        self.layer2 = torch.nn.Linear(10, 10)
+        self.layer3 = torch.nn.Linear(10, 10)
+        self.layer4 = torch.nn.Linear(10, 10)
+        self.layer5 = torch.nn.Linear(10, 10)
+        self.layer6 = torch.nn.Linear(10, 10)
+        
+    
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        return x
+        
 def test_get_sequential_ancestors():
     model = DummyModel()
 
@@ -22,3 +44,33 @@ def test_get_sequential_ancestors():
     assert get_sequential_ancestors(model, {model.fc}) == {model}
     assert get_sequential_ancestors(model, {model.seq[0]}) == {model, model.seq}
     assert get_sequential_ancestors(model, {model.seq[1]}) == {model, model.seq}
+
+def test_topological_partition_default():
+    model = DummyModelMultipleSequentialLayers()
+    targets = {model.layer1, model.layer2, model.layer3, model.layer4, model.layer5, model.layer6}
+    gm = torch.fx.symbolic_trace(model)
+    
+    
+    assert len(topological_partition(gm, targets)) == 7
+
+def test_topological_partition_multiple_targets():
+    model = DummyModelMultipleSequentialLayers()
+    gm = torch.fx.symbolic_trace(model)
+    targets = {model.layer1, model.layer2, model.layer3, model.layer4, model.layer5, model.layer6}
+
+    assert len(topological_partition(gm, targets, 2)) == 4
+
+def test_topological_partition_invalid():
+    model = DummyModelMultipleSequentialLayers()
+    gm = torch.fx.symbolic_trace(model)
+    targets = {model.layer1, model.layer2, model.layer3, model.layer4, model.layer5, model.layer6}
+
+    
+    with pytest.raises(ValueError):
+        topological_partition(gm, targets, 0)
+
+    
+    
+    
+    
+    
