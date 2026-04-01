7359a32e3f70c0060c02e7a3b68c50aba75a6796
Brian Dellabetta <bdellabe@redhat.com>
2026-04-01 19:12:43 +0000

    revert reindex function
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/llmcompressor/entrypoints/model_free/reindex_fused_weights.py
+++ src/llmcompressor/entrypoints/model_free/reindex_fused_weights.py
@@ -1,10 +1,23 @@
 import argparse
+import json
+import os
+import shutil
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 
-from compressed_tensors.entrypoints.convert import (
-    reindex_checkpoint,
+import torch
+import tqdm
+from compressed_tensors.utils.safetensors_load import (
+    find_safetensors_index_file,
+    get_checkpoint_files,
+    get_file_map,
+    is_weights_file,
+    update_safetensors_index,
 )
+from loguru import logger
+from safetensors.torch import load_file, save_file
 
-from .microscale import get_unmatched_microscale_names
+from llmcompressor.entrypoints.model_free.microscale import get_fused_names
 
 
 def parse_args():
@@ -39,12 +52,81 @@ def reindex_fused_weights(
     :param num_workers: number of worker threads to save files with
     """
 
-    reindex_checkpoint(
-        model_stub,
-        save_directory,
-        num_workers,
-        get_unmatched_names=get_unmatched_microscale_names,
-    )
+    # read files
+    model_files = get_checkpoint_files(model_stub)
+    index_file = find_safetensors_index_file(model_files)
+    if index_file is None:
+        raise ValueError(
+            "This script is used to modify safetensor file shards, but was "
+            "unable to find safetenors index file. No reindexing is required."
+        )
+
+    # copy non-weight files
+    for file_path, resolved_path in model_files.items():
+        save_path = Path(save_directory) / file_path
+
+        if file_path.endswith("safetensors"):
+            continue
+        else:
+            if is_weights_file(file_path):
+                logger.warning(f"Skip processing for weights file {file_path}")
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+            logger.debug(f"Copying {file_path} {save_path}")
+            shutil.copyfile(resolved_path, save_path)
+
+    # read index file
+    with open(index_file, "r") as file:
+        index_file_data = json.load(file)
+
+    weight_map: dict[str, str] = index_file_data["weight_map"]
+    final_weight_map: dict[str, str] = {}
+
+    # set up copy executor and carry over
+    writers = ThreadPoolExecutor(max_workers=num_workers)
+    carry_over_tensors: dict[str, torch.Tensor] = {}
+
+    # iterate in alphabetical order on assumption of weight-file locality
+    file_map = get_file_map(weight_map)
+    file_map = sorted(file_map)
+    progress = tqdm.tqdm(total=len(file_map))
+    for file_name in file_map:
+        file_path = model_files[file_name]
+        save_path = os.path.join(save_directory, file_name)
+        tensors = load_file(file_path)
+
+        if len(carry_over_tensors) > 0:
+            # add carryover
+            tensors.update(carry_over_tensors)
+            logger.info(f"Moved {list(carry_over_tensors.keys())} into {file_name}")
+            carry_over_tensors = {}
+
+        tensor_names = sorted(list(tensors.keys()))
+        _matches, unmatched_sets = get_fused_names(tensor_names)
+        for unmatched in unmatched_sets:
+            # move to carry over
+            unmatched_tensors = {
+                key: tensors[key] for key in unmatched.values() if key is not None
+            }
+            carry_over_tensors.update(unmatched_tensors)
+
+            # delete from current file
+            for key in unmatched_tensors:
+                tensor_names.remove(key)
+                del tensors[key]
+
+        # save tensors after modification
+        writers.submit(_with_progress, save_file, tensors, save_path, progress=progress)
+        final_weight_map.update({name: file_name for name in tensor_names})
+
+    total_size = index_file_data["metadata"]["total_size"]
+    update_safetensors_index(save_directory, total_size, final_weight_map)
+    writers.shutdown(wait=True)
+
+
+def _with_progress(fn: callable, *args, progress: tqdm.tqdm):
+    ret = fn(*args)
+    progress.update(1)
+    return ret
 
 
 def main():
