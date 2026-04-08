b07a131ccb7010ed9532aa72ed4ae444f2a095b3
Dipika Sikka <dipikasikka1@gmail.com>
2026-04-08 10:52:16 -0400

    Update compressed-tensors version to pick up correct nightly
    Signed-off-by: Dipika Sikka <dipikasikka1@gmail.com>

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ setup(
         (
             "compressed-tensors==0.14.0"
             if BUILD_TYPE == "release"
-            else "compressed-tensors>=0.14.1a2"
+            else "compressed-tensors>=0.15.1a2"
         ),
     ],
     extras_require={
