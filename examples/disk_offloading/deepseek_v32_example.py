b3929d5bd82b7c3a921709f08789a85f2cc82d65
Brian Dellabetta <bdellabe@redhat.com>
2026-04-08 21:19:03 +0000

    update example
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- examples/disk_offloading/deepseek_v32_example.py
+++ examples/disk_offloading/deepseek_v32_example.py
@@ -1,4 +1,10 @@
 import torch
+import json
+
+from compressed_tensors.entrypoints.convert import (
+    FP8BlockDequantizer,
+    convert_checkpoint,
+)
 from compressed_tensors.offload import get_device_map, load_offloaded_model
 from compressed_tensors.quantization.quant_scheme import (
     FP8_BLOCK,
@@ -11,33 +17,52 @@ from llmcompressor import oneshot
 from llmcompressor.modeling.deepseekv32.model import DeepseekV32ForCausalLM
 from llmcompressor.modifiers.quantization import QuantizationModifier
 
-# from llmcompressor.modeling.deepseekv32.config import (
-#     ModelConfig as DeepseekV32Config,
-# )
-# from transformers import AutoConfig, AutoModelForCausalLM
-# AutoConfig.register("deepseek_v32", DeepseekV32Config)
-# AutoModelForCausalLM.register(DeepseekV32Config, DeepseekV32ForCausalLM)
-
-
-model_id = "/mnt/data/brian-dellabetta/DeepSeek-V3.2-bf16"
-
+# `deepseek-ai/DeepSeek-V3.2` has layers that are quantized in the FP8 quant method's
+# FP8_BLOCK scheme. This example will first convert the checkpoint to bfloat16 so that
+# the model can be compressed in compressed-tensors format, in another configuration.
+MODEL_ID = "deepseek-ai/DeepSeek-V3.2"
+BFLOAT16_SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-bf16"
 SAVE_DIR = "DeepSeek-V3.2-NVFP4-FP8-BLOCK"
 
-# Select model and load it in the `load_offloaded_model` context
+# 1) Convert DeepSeek-V3.2 back to dense bfloat16 format
+convert_checkpoint(
+    model_stub=MODEL_ID,
+    save_directory=BFLOAT16_SAVE_DIR,
+    converter=FP8BlockDequantizer(
+        # `deepseek-ai/DeepSeek-V3.2` fp8-block-quantized layers, found by inspection
+        targets=[
+            r"re:.*mlp.*\.(gate_up|gate|up|down)_proj$",
+            r"re:.*self_attn.*\.(kv_b|o|q_a|q_b)_proj$",
+            r"re:.*self_attn.kv_a_proj_with_mqa$",
+            r"re:.*self_attn.indexer.(wk|wq_b)$",
+        ],
+    ),
+    max_workers=4,
+)
+
+# 2) For some reason DeepSeek splits important config info into a separate file that
+#    will break loading in transformers if not merged into config.json
+with open(f"{BFLOAT16_SAVE_DIR}/config.json", "r") as f:
+    orig_config = json.load(f)
+with open(f"{BFLOAT16_SAVE_DIR}/inference/config_671B_v3.2.json", "r") as f:
+    additional_config_data = json.load(f)
+    additional_config_data.pop("dtype")
+with open(f"{BFLOAT16_SAVE_DIR}/config.json", "w") as f:
+    config = orig_config | additional_config_data
+    json.dump(config, f)
+
+
+# 3) Apply oneshot to bfloat16 model
 with load_offloaded_model(), torch.no_grad():
     model = DeepseekV32ForCausalLM.from_pretrained(
-        model_id,
+        BFLOAT16_SAVE_DIR,
         dtype="auto",
         device_map="auto_offload",  # fit as much as possible on cpu, rest goes on disk
         trust_remote_code=True,
         offload_folder="./offload_folder",
         max_memory={"cpu": 500e9},  # don't exceed 500GB RAM
     )
-    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-
-# Confirm that model is dispatched correctly
-devices = {offloaded for _onloaded, offloaded in get_device_map(model).values()}
-print(f"Model was offloaded to the following devices: {devices}")
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, trust_remote_code=True)
 
 # Select calibration dataset.
 DATASET_ID = "ultrachat-200k"
