16802c1b538552c6526195b2295cdb9b0e9601e9
zhangxin81 <115389973+zhangxin81@users.noreply.github.com>
2026-04-20 08:41:12 +0000

    add example of w8a8fp8 for qwen3.5
    Signed-off-by: zhangxin81 <115389973+zhangxin81@users.noreply.github.com>

---
+++ examples/quantization_w8a8_fp8/qwen3_5_example.py
@@ -0,0 +1,45 @@
+import torch
+from compressed_tensors.utils import save_mtp_tensors_to_checkpoint
+from datasets import load_dataset
+from transformers import AutoProcessor, Qwen3_5MoeForConditionalGeneration
+
+from llmcompressor import oneshot
+from llmcompressor.modifiers.quantization import QuantizationModifier
+
+# NOTE: This example requires transformers >= v5
+
+MODEL_ID = "/Qwen/Qwen3.5-122B-A10B"
+
+# Load model.
+model = Qwen3_5MoeForConditionalGeneration.from_pretrained(MODEL_ID, dtype="auto")
+processor = AutoProcessor.from_pretrained(MODEL_ID)
+
+# No need to include mtp layers as they are not loaded
+# through Qwen3_5MoeForConditionalGeneration
+recipe = QuantizationModifier(
+    targets="Linear",
+    scheme="FP8_DYNAMIC",
+    ignore=[
+        "re:.*lm_head",
+        "re:visual.*",
+        "re:model.visual.*",
+        "re:.*mlp.gate$",
+        "re:.*embed_tokens$",
+        "re:.*shared_expert_gate$",
+        "re:.*linear_attn.*",
+    ],
+)
+
+# Apply quantization.
+oneshot(
+    model=model,
+    recipe=recipe)
+
+# Save to disk in compressed-tensors format.
+SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-FP8-Dynamic"
+model.save_pretrained(SAVE_DIR)
+processor.save_pretrained(SAVE_DIR)
+
+# MTP layers are excluded from the model through Qwen3_5MoeForConditionalGeneration
+# Save them as-is from the original checkpoint into the quantized output.
+save_mtp_tensors_to_checkpoint(source_model=MODEL_ID, dest_dir=SAVE_DIR)
