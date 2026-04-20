c70e6541e7520ff095dadcd675611db76bfa29b3
Debolina Roy <debroy@redhat.com>
2026-04-20 12:35:38 -0400

    Adding test_group to lm-eval configs-4
    Signed-off-by: Debolina Roy <debroy@redhat.com>

--- setup.py
+++ setup.py
@@ -123,6 +123,11 @@ setup(
         ("tqdm>=4.66.3,<=4.67.3" if BUILD_TYPE == "release" else "tqdm>=4.66.3"),
         ("torch>=2.10.0,<=2.11.0" if BUILD_TYPE == "release" else "torch>=2.10.0"),
         (
+            "torchvision>=0.24.0,<=0.27.0"
+            if BUILD_TYPE == "release"
+            else "torchvision>=0.24.0"
+        ),
+        (
             "transformers>=4.56.1,<=4.57.6"
             if BUILD_TYPE == "release"
             else "transformers>=4.56.1,<=4.57.6"
@@ -162,7 +167,6 @@ setup(
             "cmarkgfm>=2024.1.14",
             "trl>=0.10.1",
             "pandas<2.3.0",
-            "torchvision",
             "librosa==0.11.0",
             "soundfile",
             "torchcodec",
