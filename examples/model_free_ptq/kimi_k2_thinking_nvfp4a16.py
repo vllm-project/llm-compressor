59cdde28b3f69557e8f537e91016d533367e01e8
Brian Dellabetta <bdellabe@redhat.com>
2026-04-29 21:06:06 +0000

    kimi k2.6 examples
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- examples/model_free_ptq/kimi_k2_thinking_nvfp4a16.py
+++ examples/model_free_ptq/kimi_k2_thinking_nvfp4a16.py
@@ -1,27 +1,13 @@
-"""
-NOTE: Please run the following script before using `model_free_ptq`
-
-This script is used to reindex the safetensors files of a model such that all fused
-modules (gate_up, qkv) are in the same safetensors file. This is required by
-model_free_ptq for microscale schemes (NVFP4A16, MXFP4A16)
-
-llmcompressor.reindex_fused_weights \
-    unsloth/Kimi-K2-Thinking-BF16 \
-    Kimi-K2-Thinking-BF16-reindexed \
-    --num_workers=10
-"""
-
 from llmcompressor import model_free_ptq
 
 MODEL_ID = "unsloth/Kimi-K2-Thinking-BF16"
-REINDEX_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-reindexed"
 SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-NVFP4A16"
 
 # See above notice pertaining to safetensors reindexing
 # After running `llmcompressor.reindex_fused_weights`,
 # use `model_free_ptq` to apply NVFP4A16 quantization
 model_free_ptq(
-    model_stub=REINDEX_DIR,
+    model_stub=MODEL_ID,
     save_directory=SAVE_DIR,
     scheme="NVFP4A16",
     ignore=[
