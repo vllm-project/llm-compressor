cf9081134659db573f66fa5eccdcfeb2eaa32cb1
Barry Shapira <bshapira@crusoe.ai>
2026-05-10 05:10:00 -0700

    bump transformers and compressed-tensors

--- setup.py
+++ setup.py
@@ -123,9 +123,9 @@ setup(
         ("tqdm>=4.66.3,<=4.67.3" if BUILD_TYPE == "release" else "tqdm>=4.66.3"),
         ("torch>=2.10.0,<=2.11.0" if BUILD_TYPE == "release" else "torch>=2.10.0"),
         (
-            "transformers>=4.56.1,<=4.57.6"
+            "transformers>=5.0"
             if BUILD_TYPE == "release"
-            else "transformers>=4.56.1,<=4.57.6"
+            else "transformers>=5.0"
         ),
         ("datasets>=4.8.4,<=4.8.4" if BUILD_TYPE == "release" else "datasets>=4.8.4"),
         (
@@ -145,9 +145,9 @@ setup(
         ),
         ("pillow>=10.4.0,<=12.1.1" if BUILD_TYPE == "release" else "pillow>=10.4.0"),
         (
-            "compressed-tensors==0.14.0"
+            "compressed-tensors>=0.14.0"
             if BUILD_TYPE == "release"
-            else "compressed-tensors>=0.15.1a2"
+            else "compressed-tensors>=0.14.0"
         ),
     ],
     extras_require={
