ecadc6e989cde843411a2f8026682901f2f63557
Kyle Sayers <kylesayrs@gmail.com>
2026-05-20 16:15:31 -0400

    add dequantize script
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- deepseek_dequantize.py
+++ deepseek_dequantize.py
@@ -4,13 +4,12 @@ from llmcompressor.modeling.moe.linearize import linearize_moe_model
 
 model = AutoModelForCausalLM.from_pretrained(
     "deepseek-ai/DeepSeek-V4-Flash",
-    torch_dtype="auto",
     device_map="cpu",
 )
 delattr(model, "_weight_conversions")
 tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-V4-Flash")
 
 save_dir = "DeepSeek-V4-Flash-bf16"
-#model.dequantize(torch.bfloat16)
+# model.dequantize(torch.bfloat16)
 model.save_pretrained(save_dir)
 tokenizer.save_pretrained(save_dir)
