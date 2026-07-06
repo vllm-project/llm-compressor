fbe7c991b10a84292649373a1b52c4a4a9164d7c
Kyle Sayers <kylesayrs@gmail.com>
2026-07-06 16:22:12 -0400

    fix
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- examples/multimodal_vision/internvl3_example.py
+++ examples/multimodal_vision/internvl3_example.py
@@ -1,5 +1,3 @@
-import torch
-from datasets import load_dataset
 from transformers import AutoModelForImageTextToText, AutoProcessor
 
 from llmcompressor import oneshot
@@ -7,47 +5,19 @@ from llmcompressor.modifiers.gptq import GPTQModifier
 
 # Load model.
 model_id = "OpenGVLab/InternVL3-8B-hf"
-model = AutoModelForImageTextToText.from_pretrained(model_id, dtype=torch.bfloat16)
+model = AutoModelForImageTextToText.from_pretrained(model_id)
 processor = AutoProcessor.from_pretrained(model_id)
 
-# Load datasets
-DATASET_ID = "HuggingFaceH4/ultrachat_200k"
-DATASET_SPLIT = "train_sft"
-NUM_CALIBRATION_SAMPLES = 256
-MAX_SEQUENCE_LENGTH = 512
-ds = load_dataset(DATASET_ID, split=f"{DATASET_SPLIT}[:{NUM_CALIBRATION_SAMPLES}]")
-ds = ds.shuffle(seed=42)
-
-
-def preprocess_and_tokenize(example):
-    messages = [
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": example["messages"]},
-            ],
-        }
-    ]
-    inputs = processor.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        tokenize=True,
-        return_dict=True,
-        return_tensors="pt",
-    )
-
-    # remove extra dim added by multimodal processors
-    inputs = {key: value[0] for key, value in inputs.items()}
-
-    return inputs
-
-
-ds = ds.map(preprocess_and_tokenize, remove_columns=ds.column_names)
+# Oneshot arguments
+DATASET_ID = "flickr30k"
+DATASET_SPLIT = "test"
+NUM_CALIBRATION_SAMPLES = 512
+MAX_SEQUENCE_LENGTH = 2048
 
 # Recipe
 recipe = GPTQModifier(
     targets="Linear",
-    scheme="FP8",
+    scheme="NVFP4",
     ignore=["re:.*lm_head", "re:.*vision_tower.*", "re:.*multi_modal_projector.*"],
 )
 
@@ -55,13 +25,14 @@ recipe = GPTQModifier(
 oneshot(
     model=model,
     tokenizer=model_id,
-    dataset=ds,
+    dataset=DATASET_ID,
+    splits=f"{DATASET_SPLIT}[:{NUM_CALIBRATION_SAMPLES}]",
     recipe=recipe,
     max_seq_length=MAX_SEQUENCE_LENGTH,
     num_calibration_samples=NUM_CALIBRATION_SAMPLES,
 )
 
 # Save to disk compressed.
-SAVE_DIR = model_id.rstrip("/").split("/")[-1] + "-FP8"
+SAVE_DIR = model_id.rstrip("/").split("/")[-1] + "-NVFP4"
 model.save_pretrained(SAVE_DIR, save_compressed=True)
 processor.save_pretrained(SAVE_DIR)
