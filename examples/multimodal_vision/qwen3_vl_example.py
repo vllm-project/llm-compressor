85aca66e68bcdb0cdcf09ca83491c68e75c9a4b7
Kyle Sayers <kylesayrs@gmail.com>
2026-07-21 15:35:35 -0400

    add example
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- examples/multimodal_vision/qwen3_vl_example.py
+++ examples/multimodal_vision/qwen3_vl_example.py
@@ -3,13 +3,13 @@ from io import BytesIO
 
 import torch
 from compressed_tensors.offload import dispatch_model
-from datasets import load_dataset
 
 # Note: this is an optional utility for processing vision inputs for qwen.
 # This can be installed via the "qwen" extra
 from qwen_vl_utils import process_vision_info
 from transformers import AutoProcessor, Qwen3VLForConditionalGeneration
 
+from datasets import load_dataset
 from llmcompressor import oneshot
 from llmcompressor.modifiers.quantization import QuantizationModifier
 from llmcompressor.modifiers.transform.awq import AWQModifier
