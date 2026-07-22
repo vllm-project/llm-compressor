f000f1e124b7760025c16898cb8a510907381a41
Kyle Sayers <kylesayrs@gmail.com>
2026-07-22 19:35:01 -0400

    [Example] [MoE] HY_V3 (#2928)
    ## Purpose ##
    * Support `HY_V3` model
    
    ## Prerequisites ##
    ### Transformers ###
    - [ ] https://github.com/huggingface/transformers/pull/47336
    
    ## Testing ##
    * Used to generate https://huggingface.co/RedHatAI/Hy3-NVFP4-FP8
    
    ---------
    
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

---
+++ examples/quantization_w4a4_fp4/hy3_example.py
@@ -0,0 +1,99 @@
+import torch
+from compressed_tensors.distributed import init_dist
+from compressed_tensors.quantization.quant_scheme import (
+    FP8_BLOCK,
+    NVFP4,
+    QuantizationScheme,
+)
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from llmcompressor import oneshot
+from llmcompressor.datasets.utils import get_rank_partition
+from llmcompressor.modifiers.quantization import QuantizationModifier
+from llmcompressor.utils import load_context
+
+# Select model and load it.
+MODEL_ID = "tencent/Hy3"
+
+init_dist()
+with load_context():
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_ID,
+        device_map="auto_offload",
+        max_memory={},
+        offload_folder="offload_folder",
+    )
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+
+# Select calibration dataset.
+DATASET_ID = "HuggingFaceH4/ultrachat_200k"
+DATASET_SPLIT = "train_sft"
+
+# Select number of samples. 512 samples is a good place to start.
+# Increasing the number of samples can improve accuracy.
+NUM_CALIBRATION_SAMPLES = 512
+MAX_SEQUENCE_LENGTH = 2048
+
+# Load dataset and preprocess.
+ds = load_dataset(
+    DATASET_ID, split=get_rank_partition(DATASET_SPLIT, NUM_CALIBRATION_SAMPLES)
+)
+ds = ds.shuffle(seed=42)
+
+
+def preprocess(example):
+    return {
+        "text": tokenizer.apply_chat_template(
+            example["messages"],
+            tokenize=False,
+        )
+    }
+
+
+ds = ds.map(preprocess)
+
+
+# Tokenize inputs.
+def tokenize(sample):
+    return tokenizer(
+        sample["text"],
+        padding=False,
+        max_length=MAX_SEQUENCE_LENGTH,
+        truncation=True,
+        add_special_tokens=False,
+    )
+
+
+ds = ds.map(tokenize, remove_columns=ds.column_names)
+
+recipe = QuantizationModifier(
+    config_groups={
+        "attention": QuantizationScheme(
+            targets=[r"re:.*self_attn\..*"],
+            **FP8_BLOCK,
+        ),
+        "experts": QuantizationScheme(
+            targets=[r"re:.*mlp.*"],
+            **NVFP4,
+        ),
+    },
+    ignore=["lm_head"],
+)
+
+# Apply algorithms.
+oneshot(
+    model=model,
+    processor=tokenizer,
+    dataset=ds,
+    recipe=recipe,
+    batch_size=4,
+    shuffle_calibration_samples=False,
+)
+
+# Save to disk compressed.
+SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-NVFP4-FP8"
+model.save_pretrained(SAVE_DIR, save_compressed=True)
+tokenizer.save_pretrained(SAVE_DIR)
+
+torch.distributed.destroy_process_group()
