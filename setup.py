1147b90811a22740e86803b02ad89d2a0857f91a
Dan Huang <dahuang@redhat.com>
2026-07-23 11:31:14 -0400

    update pillow upper bound
    Signed-off-by: Dan Huang <dahuang@redhat.com>

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
             if BUILD_TYPE == "release"
             else "nvidia-ml-py>=12.560.30"
         ),
-        ("pillow>=10.4.0,<=12.2.0" if BUILD_TYPE == "release" else "pillow>=10.4.0"),
+        ("pillow>=10.4.0,<=12.3.0" if BUILD_TYPE == "release" else "pillow>=10.4.0"),
         (
             "compressed-tensors==0.17.1"
             if BUILD_TYPE == "release"
