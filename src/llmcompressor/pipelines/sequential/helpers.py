11206f23e8b131e85ca3b03145e3eac447afc7b6
YingqiDuan <141370165+YingqiDuan@users.noreply.github.com>
2026-08-01 18:59:37 -0700

    Optimize consumed name tracing
    Signed-off-by: YingqiDuan <141370165+YingqiDuan@users.noreply.github.com>

--- src/llmcompressor/pipelines/sequential/helpers.py
+++ src/llmcompressor/pipelines/sequential/helpers.py
@@ -402,16 +402,11 @@ def trace_consumed_names(subgraphs: list[Subgraph]):
 
     :param subgraphs: list of subgraphs with empty `consumed_names` attributes
     """
-    # populate consumed_names according to when inputs are last used
-    # in order to vacate the `intermediates` cache and save memory
-    all_input_names = set().union(*(subgraph.input_names for subgraph in subgraphs))
-    for input_name in all_input_names:
-        for subgraph in reversed(subgraphs):
-            if input_name in subgraph.input_names:
-                subgraph.consumed_names.add(input_name)
-                break
-        else:
-            raise ValueError(f"Could not find input name {input_name} in subgraphs")
+    # The first occurrence in reverse order is the final use in execution order.
+    seen_names: set[str] = set()
+    for subgraph in reversed(subgraphs):
+        subgraph.consumed_names.update(subgraph.input_names - seen_names)
+        seen_names.update(subgraph.input_names)
 
 
 def graph_is_well_formed(graph: Graph) -> bool:
