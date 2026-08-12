b3da4c46f25c8155ed18e18ae6c9335de99572b9
Kyle Sayers <kylesayrs@gmail.com>
2026-08-12 19:10:45 +0000

    tmp
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/llmcompressor/transformers/compression/compressed_tensors_utils.py
+++ src/llmcompressor/transformers/compression/compressed_tensors_utils.py
@@ -239,7 +239,7 @@ def update_and_save_recipe(model_stub: str, save_directory: str):
 
 @contextmanager
 def suspend_distributed_timeout(
-    timeout: datetime.timedelta = datetime.timedelta(hours=3),
+    timeout: datetime.timedelta = datetime.timedelta(hours=12),
     current_group: dist.ProcessGroup | None = None,
 ):
     """
