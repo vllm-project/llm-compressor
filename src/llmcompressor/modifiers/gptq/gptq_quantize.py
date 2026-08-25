09ed14efd44a98a88139adf61bff2eabc6177dd3
Roderick Wu <rowu@redhat.com>
2026-08-25 14:14:48 +0000

    move actorder validate to ct
    Signed-off-by: Roderick Wu <rowu@redhat.com>

--- src/llmcompressor/modifiers/gptq/gptq_quantize.py
+++ src/llmcompressor/modifiers/gptq/gptq_quantize.py
@@ -96,8 +96,6 @@ def quantize_weight(
 
     # handle activation ordering
     if actorder:
-        aliases = ActivationOrdering.get_aliases()
-        actorder = actorder if actorder not in aliases else aliases[actorder]
         W, H, perm = _apply_activation_ordering(W, H)
 
     # handle g_idx
