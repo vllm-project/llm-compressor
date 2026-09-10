926b652bc99376e145c24ad13be6c1a06275d30b
Kyle Sayers <kylesayrs@gmail.com>
2026-09-10 22:24:41 +0000

    touch up examples
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- examples/model_free_ptq/decompress_glm53_flash.py
+++ examples/model_free_ptq/decompress_glm53_flash.py
@@ -2,17 +2,13 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 from compressed_tensors.entrypoints.convert import (
-    convert_checkpoint,
     FP8BlockDequantizer,
+    convert_checkpoint,
 )
 
-# deepseek-ai/DeepSeek-V3.2 checkpoint has layers that are quantized in the FP8
-# quant method's FP8_BLOCK scheme. This script will upconvert to bfloat16 so that
-# the model can be compressed in another configuration.
 MODEL_ID = "zai-org/GLM-5.3-Flash"
 SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-BF16"
 
-# Convert DeepSeek-V3.2 back to dense bfloat16 format
 convert_checkpoint(
     model_stub=MODEL_ID,
     save_directory=SAVE_DIR,
