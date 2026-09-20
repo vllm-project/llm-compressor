9bad91bb52bc62316885ec7224227fa173243641
weimin cai <weimin.cai@intel.com>
2026-09-20 13:25:12 +0800

    fix: disable Triton GPTQ backend on XPU
    Signed-off-by: weimin cai <weimin.cai@intel.com>

--- .buildkite/xpu-tests/scripts/run-tests-xpu.sh
+++ .buildkite/xpu-tests/scripts/run-tests-xpu.sh
@@ -37,6 +37,6 @@ docker exec -e NUMA_NODE=${NUMA_NODE} -e NUMA_CPUSET=${NUMA_CPUSET} -e ZE_AFFINI
 
 echo "--- Run XPU E2E tests..."
 docker exec -e NUMA_NODE=${NUMA_NODE} -e NUMA_CPUSET=${NUMA_CPUSET} -e ZE_AFFINITY_MASK=${ZE_AFFINITY_MASK} "$CONTAINER_NAME" \
-              bash -c "export CADENCE=nightly SKIP_HF_UPLOAD=yes; \
+              bash -c "export CADENCE=nightly SKIP_HF_UPLOAD=yes LLMCOMPRESSOR_DISABLE_GPTQ_TRITON=1; \
               numactl --physcpubind=${NUMA_CPUSET:-84-111} --membind=${NUMA_NODE:-3} \
               bash tests/e2e/run_tests_in_python.sh -c tests/e2e/configs -f w4a16_grouped_quant"
