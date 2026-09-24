06af2de42bc6814d2dc8e22d0e82dfe1e97f7a56
Roderick Wu <rowu@redhat.com>
2026-09-24 04:03:51 +0000

    lint and nits Signed-off-by: Roderick Wu <rowu@redhat.com>

--- src/llmcompressor/pipelines/sequential/pipeline.py
+++ src/llmcompressor/pipelines/sequential/pipeline.py
@@ -151,48 +151,46 @@ class SequentialPipeline(CalibrationPipeline):
                 ### START OF ONLOAD ###
                 #######################
                 offload_kwargs = {}
-                try:
-                    if sequential_offload_pinned_memory and onload_device.type != "cpu":
-                        stage_modules(subgraph_modules, pin_memory=True)
-                    offload_kwargs = onload_modules(subgraph_modules)
+                if sequential_offload_pinned_memory and onload_device.type != "cpu":
+                    stage_modules(
+                        subgraph_modules, pin_memory=sequential_offload_pinned_memory
+                    )
+                offload_kwargs = onload_modules(subgraph_modules)
 
-                    # do a preliminary pass to trigger modifier hooks
-                    for batch_idx, inputs in _get_batches(
-                        activations,
-                        num_batches,
-                        subgraph.input_names,
-                        calib_desc,
-                        sequential_prefetch,
-                    ):
-                        session.state.current_batch_idx = batch_idx
-                        outputs = subgraph.forward(model, **inputs)
+                # do a preliminary pass to trigger modifier hooks
+                for batch_idx, inputs in _get_batches(
+                    activations,
+                    num_batches,
+                    subgraph.input_names,
+                    calib_desc,
+                    sequential_prefetch,
+                ):
+                    session.state.current_batch_idx = batch_idx
+                    outputs = subgraph.forward(model, **inputs)
 
-                        if not dataset_args.propagate_error:
+                    if not dataset_args.propagate_error:
+                        if subgraph_index < num_subgraphs - 1:
+                            activations.update(batch_idx, outputs)
+                            activations.delete(batch_idx, subgraph.consumed_names)
+
+                LifecycleCallbacks.sequential_epoch_end(subgraph.submodules(model))
+
+                if dataset_args.propagate_error:
+                    # this pass does not trigger modifier hooks
+                    # and is only used for capturing outputs of compressed modules
+                    with HooksMixin.disable_hooks():
+                        for batch_idx, inputs in _get_batches(
+                            activations,
+                            num_batches,
+                            subgraph.input_names,
+                            prop_desc,
+                            sequential_prefetch,
+                        ):
+                            output = subgraph.forward(model, **inputs)
                             if subgraph_index < num_subgraphs - 1:
-                                activations.update(batch_idx, outputs)
+                                activations.update(batch_idx, output)
                                 activations.delete(batch_idx, subgraph.consumed_names)
-
-                    LifecycleCallbacks.sequential_epoch_end(subgraph.submodules(model))
-
-                    if dataset_args.propagate_error:
-                        # this pass does not trigger modifier hooks
-                        # and is only used for capturing outputs of compressed modules
-                        with HooksMixin.disable_hooks():
-                            for batch_idx, inputs in _get_batches(
-                                activations,
-                                num_batches,
-                                subgraph.input_names,
-                                prop_desc,
-                                sequential_prefetch,
-                            ):
-                                output = subgraph.forward(model, **inputs)
-                                if subgraph_index < num_subgraphs - 1:
-                                    activations.update(batch_idx, output)
-                                    activations.delete(
-                                        batch_idx, subgraph.consumed_names
-                                    )
-                finally:
-                    offload_modules(subgraph_modules, offload_kwargs)
+                offload_modules(subgraph_modules, offload_kwargs)
                 #######################
                 #### END OF ONLOAD ####
                 #######################
